8dbfd9fbaf767449b297ab50444a6e61f9000145
Ramón Casero <rcasero@gmail.com>
2009-09-07 22:49:33 +0000

    Add initial files to start program extractVoxelCoordinatesFromSegmentationMask

---
+++ cpp/ExtractVoxelCoordinatesFromSegmentationMask.cxx
@@ -0,0 +1,15 @@
+/*
+ * ExtractVoxelCoordinatesFromSegmentationMask.cxx
+ *
+ * Program that finds the voxels set to true in a segmentation mask,
+ * and outputs a matrix with their coordinates
+ * 
+ */ 
+
+#ifdef _MSC_VER
+#pragma warning ( disable : 4786 )
+#endif
+
+// C++ functions
+#include <iostream>
+
