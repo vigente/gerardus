fd7e83af1d024adcd91bef361dd10c5125ff0d93
rcasero <rcasero@e9491f86-7216-11de-921c-dbe4921c5040>
2010-02-09 01:18:59 +0000

    * FiltersToolbox/scinrrd_ndgrid.m:
    	- Fix bug. Limits for x-coordinates were used for y-coordinates
    	and viceversa.

--- trunk/matlab/FiltersToolbox/scinrrd_ndgrid.m
+++ trunk/matlab/FiltersToolbox/scinrrd_ndgrid.m
@@ -53,5 +53,5 @@ res = [ nrrd.axis.spacing ];
 % generate 3D grid of coordinates
 % note the inversion of x and y so that x values change with columns, and y
 % values change with rows
-[ yg, xg, zg ] = ndgrid( vmin(1):res(1):vmax(1), ...
-    vmin(2):res(2):vmax(2), vmin(3):res(3):vmax(3) );
+[ yg, xg, zg ] = ndgrid( vmin(2):res(2):vmax(2), ...
+    vmin(1):res(1):vmax(1), vmin(3):res(3):vmax(3) );
