945d4246db909e7a22d7cf8d3431128ce30ec02d
Ramón Casero <rcasero@gmail.com>
2011-03-30 19:08:27 +0000

    * ExtractVoxelCoordinatesFromSegmentationMask.cxx, 	PadSegmentationMaskWithVoxels.cxx, Resize3DImage.cxx: Add author.
    	* CMakeLists.txt: Add comment licence headers.
    
    	* Resize3DImage.cxx: Add comment and licence headers.

--- cpp/src/Resize3DImage.cxx
+++ cpp/src/Resize3DImage.cxx
@@ -1,3 +1,50 @@
+/*
+ * Resize3DImage.cxx
+ *
+ * Program to resize a 3D image 
+ * 
+ * Example of usage:
+ * 
+ *  $ ./resize3DImage 512 640 1024 image.mha 
+ * 
+ * This resizes the 3D image contained in image.mha to an output size
+ * of 512 x 640 x 1024 voxels.
+ * 
+ * The results are saved to file image-resized.mha by default,
+ * although it's possible to specify the output file name with
+ * argument -o --outfile.
+ * 
+ */ 
+ 
+ /*
+  * Author: Ramon Casero <rcasero@gmail.com>
+  * Copyright © 2010 University of Oxford
+  *
+  * University of Oxford means the Chancellor, Masters and Scholars of
+  * the University of Oxford, having an administrative office at
+  * Wellington Square, Oxford OX1 2JD, UK. 
+  *
+  * This file is part of Gerardus.
+  *
+  * This program is free software: you can redistribute it and/or modify
+  * it under the terms of the GNU General Public License as published by
+  * the Free Software Foundation, either version 3 of the License, or
+  * (at your option) any later version.
+  *
+  * This program is distributed in the hope that it will be useful,
+  * but WITHOUT ANY WARRANTY; without even the implied warranty of
+  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  * GNU General Public License for more details. The offer of this
+  * program under the terms of the License is subject to the License
+  * being interpreted in accordance with English Law and subject to any
+  * action against the University of Oxford being under the jurisdiction
+  * of the English Courts.
+  *
+  * You should have received a copy of the GNU General Public License
+  * along with this program.  If not, see
+  * <http://www.gnu.org/licenses/>.
+  */
+ 
 #ifdef _MSC_VER
 #pragma warning ( disable : 4786 )
 #endif
