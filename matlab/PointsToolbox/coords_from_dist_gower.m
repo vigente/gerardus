5386788f508593c278516b34fd1c3989e759cb14
Ramón Casero <rcasero@gmail.com>
2011-04-13 11:21:47 +0000

    * PointsToolbox/coords_from_dist_gower.m: v0.2.0
    	- Add collinearity test.

--- matlab/PointsToolbox/coords_from_dist_gower.m
+++ matlab/PointsToolbox/coords_from_dist_gower.m
@@ -1,22 +1,31 @@
-function z = coords_from_dist_gower(x, d)
+function z = coords_from_dist_gower(x, d, tol)
 % COORDS_FROM_DIST_GOWER  Compute point coordinates from distances to a set
 % of landmarks, as described in Gower (1986)
 %
 % Z = COORDS_FROM_DIST_GOWER(X, D)
 %
 %   For 2D points, X is a (2, P)-matrix with the coordinates of P
-%   landmarks. (Note that points can have any dimension.)
+%   landmarks. Points can have any dimension, they are not limited to being
+%   2D. Note that if the points in X are collinear, the result Z will be
+%   wrong in general, as the solution is not unique.
 %
 %   D is a P-vector with the distance of point Z to each of the landmarks.
+%   D can also be a matrix, if each column is a vector of distances.
 %
 %   Gower(1986) showed that the coordinates of Z can be computed from X and
 %   D.
+%
+% Z = COORDS_FROM_DIST_GOWER(X, D, TOL)
+%
+%   TOL is the tolerance value for the collinearity test. By default,
+%   TOL=1e-16. If TOL=[], then the collinearity test is skipped.
 %
 % J.C. Gower. Adding a point to vector diagrams in multivariate analysis.
 % Biometrika, 55(3):582–585, 1968.
 
 % Author: Ramon Casero <rcasero@gmail.com>
-% Copyright © 2010 University of Oxford
+% Copyright © 2010-2011 University of Oxford
+% Version: 0.2.0
 % 
 % University of Oxford means the Chancellor, Masters and Scholars of
 % the University of Oxford, having an administrative office at
@@ -42,11 +51,21 @@ function z = coords_from_dist_gower(x, d)
 % along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 % check arguments
-error( nargchk( 2, 2, nargin, 'struct' ) );
-error( nargoutchk( 0, 1, nargout, 'struct' ) );
+error(nargchk(2, 3, nargin, 'struct'));
+error(nargoutchk(0, 1, nargout, 'struct'));
 
 if (size(d, 1) ~= size(x, 2))
     error('Vertical distance vector d must have a value for each landmark')
+end
+
+% defaults
+if (nargin < 3)
+    tol = 1e-16;
+end
+
+% test for collinearity (skip test if tol=[])
+if (~isempty(tol) && iscollinear(x, tol))
+    error('X points are collinear, output coordinates cannot be determined')
 end
 
 % compute centroid of landmarks
