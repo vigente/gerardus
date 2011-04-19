0f4b4c972696e94118229686e555f05275b0270c
Ramón Casero <rcasero@gmail.com>
2011-04-19 11:20:30 +0000

    * FiltersToolbox/skeleton_plot.m: v0.2.0
    	- Add input arguments varargin so that we can pass extra options
    	to the plots.

--- matlab/FiltersToolbox/skeleton_plot.m
+++ matlab/FiltersToolbox/skeleton_plot.m
@@ -1,4 +1,4 @@
-function skeleton_plot(cc, lab)
+function skeleton_plot(cc, lab, varargin)
 % SKELETON_PLOT  Plot a labelled segmentation and its skeleton
 %
 % SKELETON_PLOT(CC, LAB)
@@ -10,11 +10,16 @@ function skeleton_plot(cc, lab)
 %   point). If LAB is provided and not empty, the branches are plotted on
 %   the labelled segmentation.
 %
+% SKELETON_PLOT(CC, LAB, ...)
+%
+%   It is possible to add more input arguments, that will be passed to the
+%   plot functions. See 'help plot' for the available options.
+%
 % See also: skeleton_label.
 
 % Author: Ramon Casero <rcasero@gmail.com>
 % Copyright © 2011 University of Oxford
-% Version: 0.1.0
+% Version: 0.2.0
 % 
 % University of Oxford means the Chancellor, Masters and Scholars of
 % the University of Oxford, having an administrative office at
@@ -40,7 +45,7 @@ function skeleton_plot(cc, lab)
 % along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 % check arguments
-error(nargchk(1, 2, nargin, 'struct'));
+error(nargchk(1, Inf, nargin, 'struct'));
 error(nargoutchk(0, 1, nargout, 'struct'));
 
 % defaults
@@ -68,21 +73,21 @@ for I = 1:cc.NumObjects
         [r, c] = ind2sub(cc.ImageSize, br);
         
         % plot branch in order
-        plot(c, r, col)
+        plot(c, r, col, varargin{:})
         hold on
         
         % plot starting and end points
-        plot(c([1 end]), r([1 end]), [col 'o'])
+        plot(c([1 end]), r([1 end]), [col 'o'], varargin{:})
     elseif (length(cc.ImageSize) == 3)
         % convert image linear indices to image r, c, s indices
         [r, c, s] = ind2sub(cc.ImageSize, br);
         
         % plot branch in order
-        plot3(c, r, s, 'w')
+        plot3(c, r, s, 'w', varargin{:})
         hold on
         
         % plot starting and end points
-        plot(c([1 end]), r([1 end]), s([1 end]), [col 'o'])
+        plot3(c([1 end]), r([1 end]), s([1 end]), [col 'o'], varargin{:})
     else 
         error('Can only print 2D or 3D skeletons')
     end
