e0958e93538d33c7decb70910668a968af5e0784
rcasero <rcasero@e9491f86-7216-11de-921c-dbe4921c5040>
2011-05-15 11:10:51 +0000

    minor

--- trunk/matlab/ItkToolbox/ItkImFilter.cpp
+++ trunk/matlab/ItkToolbox/ItkImFilter.cpp
@@ -90,7 +90,7 @@
   * Author: Ramon Casero <rcasero@gmail.com>
   * Copyright © 2011 University of Oxford
   * Version: 0.3.4
-  * Revision: $Rev$
+  * $Rev$
   *
   * University of Oxford means the Chancellor, Masters and Scholars of
   * the University of Oxford, having an administrative office at
