39c384f8f7a45832102ffe0450e75ea42b6bbdb7
Ramón Casero <rcasero@gmail.com>
2011-05-27 10:44:34 +0000

    * FiltersToolbox/scinrrd_skeleton_prune.m:
    	- (v0.2.0) Add code to prune spurious branches that are roughly as
    	long as the local radius of the main branch they are attached to.

--- matlab/FiltersToolbox/scinrrd_skeleton_prune.m
+++ matlab/FiltersToolbox/scinrrd_skeleton_prune.m
@@ -1,17 +1,41 @@
-function nrrdsk = scinrrd_skeleton_prune(nrrdsk, cc, minlen)
+function nrrdsk = scinrrd_skeleton_prune(nrrdsk, nrrd, minlen, lratio)
 % SCINRRD_SKELETON_PRUNE  Prune branches in a segmentation's skeletonization
 %
-% NRRDPR = SCINRRD_SKELETON_PRUNE(NRRDSK, CC, MINLEN)
+% This function prunes the leaves of a segmentation skeleton in two steps:
 %
-%   NRRDSK is an SCI NRRD struct. NRRD.data contains the result of running
-%   a skeletonization algorithm on a binary segmentation, e.g.
+%   1. Remove very short leaves
 %
+%   2. Remove branches created by artifacts in the segmentation. Spurious
+%      branches are those roughly as long as the local radius of the main
+%      branch they are attached to
+%
+%
+% NRRDPR = SCINRRD_SKELETON_PRUNE(NRRDSK, [], MINLEN)
+%
+%   This syntax runs step 1 only.
+%
+%   NRRDSK is an SCI NRRD struct. NRRDSK.data contains the result of
+%   running a skeletonization algorithm on a binary segmentation NRRD,
+%   e.g.
+%
+%     >> nrrdsk = nrrd;
 %     >> nrrdsk.data = itk_imfilter('skel', nrrd);
 %
-%   CC is a struct produced by function skeleton_label().
-%
 %   MINLEN is a scalar with the minimum length in voxels for a leaf. Any
-%   leaf shorter than MINLEN will be pruned.
+%   leaf shorter than MINLEN will be pruned. By default, MINLEN = 5 voxel.
+%
+%
+% NRRDPR = SCINRRD_SKELETON_PRUNE(NRRDSK, NRRD, MINLEN, LRATIO)
+%
+%   This syntax runs steps 1 and 2.
+%
+%   NRRD is the SCI NRRD struct mentioned above.
+%
+%   LRATIO is a scalar. Branches with BL/R < LRATIO will be pruned, where
+%   BL is the skeleton branch length and R is the estimated maximum radius
+%   of the main branch the leaf is connected to.
+%
+%
 %
 %   Note on SCI NRRD: Software applications developed at the University of
 %   Utah Scientific Computing and Imaging (SCI) Institute, e.g. Seg3D,
@@ -31,7 +55,7 @@ function nrrdsk = scinrrd_skeleton_prune(nrrdsk, cc, minlen)
 
 % Author: Ramon Casero <rcasero@gmail.com>
 % Copyright © 2011 University of Oxford
-% Version: 0.1.1
+% Version: 0.2.0
 % $Rev$
 % $Date$
 % 
@@ -59,19 +83,133 @@ function nrrdsk = scinrrd_skeleton_prune(nrrdsk, cc, minlen)
 % along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 % check arguments
-error(nargchk(3, 3, nargin, 'struct'));
+error(nargchk(3, 4, nargin, 'struct'));
 error(nargoutchk(0, 1, nargout, 'struct'));
 
 % defaults
-if (isempty(minlen))
-    minlen = 0;
+if (nargin < 3 || isempty(minlen))
+    minlen = 5;
 end
+if (nargin < 4 || isempty(lratio))
+    lratio = 1.2;
+end
+
+%% Step 1: pruning of very short leaf branches
+
+% split skeleton in branches
+[~, cc] = skeleton_label(nrrdsk.data, [], [nrrd.axis.spacing]);
 
 % loop each branch
 for I = 1:cc.NumObjects
     
     % if the branch needs to be pruned ...
     if (cc.IsLeaf(I) && length(cc.PixelIdxList{I}) < minlen)
+        
+        % indices of voxels in the branch that are not bifurcation voxels
+        idx = cc.PixelIdxList{I}(cc.Degree{I} < 3);
+        
+        % remove the branch voxels
+        nrrdsk.data(idx) = 0;
+    end
+    
+end
+
+%% Step 2: pruning of leaf branches produced by segmentation artifacts
+
+% skip if no full segmentation is provided
+if isempty(nrrd)
+    return
+end
+
+% separate segmentation in branch candidates using the cleaned up skeleton
+[~, cc] = skeleton_label(nrrdsk.data, nrrd.data, [nrrd.axis.spacing]);
+
+% init vector for maximum eigenvalues
+maxeigd = zeros(1, cc.NumObjects);
+
+% loop each branch
+for I = 1:cc.NumObjects
+    
+    % get index of voxel that connects this branch to other branches
+    idx0 = cc.PixelIdxList{I}(cc.Degree{I} > 2);
+    
+    % we are only interested in leaves that are attached to other branches
+    % as potential candidates for pruning
+    if (~cc.IsLeaf(I) || isempty(idx0))
+        continue
+    end
+    
+    % real world coordinates of the voxel
+    [r, c, s] = ind2sub(size(nrrd.data), idx0);
+    xyz0 = scinrrd_index2world([r, c, s], nrrd.axis);
+
+    % labels that are connected to this leaf
+    idx = cc.BranchNeighbours{I};
+    if (isempty(idx)) % skip if this branch is not connected
+        continue
+    end
+    
+    % coordinates of skeleton points that are connected to this leaf
+    idx = unique(cat(1, cc.PixelIdxList{idx}));
+
+    [r, c, s] = ind2sub(size(nrrd.data), idx);
+    xyz = scinrrd_index2world([r, c, s], nrrd.axis);
+
+    % compute eigenvectors of the neighbour branches' skeletons. We are
+    % going to use the largest as an estimate of the main vessels direction
+    eigv = pts_pca(xyz');
+    
+    % intersect 3D image with a plane that contains the first skeleton
+    % voxel, and that is orthogonal to the main vessel
+    [im, gx, gy, gz, xyz0idx] = scinrrd_intersect_plane(nrrd, xyz0, ...
+        eigv(:, 1));
+    
+    % r, c index => linear index
+    % indices in im of the rotation voxel
+    xyz0idx = sub2ind(size(im), xyz0idx(1), xyz0idx(2));
+    
+    % convert NaN to background voxels
+    im(isnan(im)) = 0;
+    
+    % compute connected components on the intersecting plane
+    ccim = bwconncomp(im);
+    
+    % find the connected component that contains the bifurcation voxel
+    FOUND = false;
+    for CCI = 1:ccim.NumObjects
+        if (any(ccim.PixelIdxList{CCI} == xyz0idx))
+            FOUND = true;
+            break
+        end
+    end
+    
+    if (~FOUND)
+        error('Assertion fail: Root voxel not found')
+    end
+    
+    % extract coordinates of voxels that belong to the connected component
+    % from above
+    xyz = [gx(ccim.PixelIdxList{CCI}), ...
+        gy(ccim.PixelIdxList{CCI}), ...
+        gz(ccim.PixelIdxList{CCI})];
+    
+    % compute PCA on the intersected voxel coordinates
+    [~, eigd] = pts_pca(xyz');
+    
+    % we are just interested in the largest eigenvalue, that will allow us
+    % to estimate the maximum thickness of the vessel at this point
+    maxeigd(I) = eigd(1);
+    
+end
+
+% list of branches that we want to prune
+idxprune = (cc.BranchLenght ./ sqrt(16*maxeigd) < lratio) & cc.IsLeaf;
+
+% loop each branch
+for I = 1:cc.NumObjects
+    
+    % if the branch needs to be pruned ...
+    if (idxprune(I))
         
         % indices of voxels in the branch that are not bifurcation voxels
         idx = cc.PixelIdxList{I}(cc.Degree{I} < 3);
