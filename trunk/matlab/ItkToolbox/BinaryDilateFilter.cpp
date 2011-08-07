da391a66704a9d2bdb37054e2b0ef9ff19b4f8f2
rcasero <rcasero@e9491f86-7216-11de-921c-dbe4921c5040>
2011-08-07 21:35:44 +0000

    * ItkToolbox/CMakeLists.txt:
    	- Add BinaryDilateFilter.cpp to project itk_imfilter().
    
    	* ItkToolbox/GerardusCommon.hpp:
    
    	- (v0.3.0) Add functions to test templates from BaseFilter.hpp.
    
    	* ItkToolbox/BaseFilter.hpp:
    
    	- (v0.4.0) Add support for user-provided parameters for
    	filters. Number of parameters and list of parameters is kept here,
    	and can be accessed by any filter derived class. Also, write a new
    	member function getScalarParamValue() to read scalar parameters
    	from Matlab (for the moment, only scalar parameters are supported,
    	and not vectors of parameters).
    
    	- Move functions to test templates to GerardusCommon.hpp.
    
    	* ItkToolbox/itk_imfilter.m:
    
    	- (v0.4.0) Correct help header to make it easier to write the
    	documentation for filters with user-provided parameters.
    
    	- Add syntax for 'bwdilate'.
    
    	* ItkToolbox/ItkImFilter.cpp (v0.4.0):
    
    	- Add support for itk::BinaryDilateImageFilter.
    
    	- Make 2 the minimum number of input arguments, not the exact
    	number. This way, the user can provide input parameters for
    	specific filters.
    
    	- Fix bug introduced in last revision. We were reading the image
    	from argIn[1], which doesn't work if the input data is a NRRD
    	struct.
    
    	- Pedantic fix. Use mxLogical instead of bool (although at least
    	for linux they are the same type).
    
    	* Add ItkToolbox/BinaryDilateFilter.cpp,
    	ItkToolbox/BinaryDilateFilter.hpp:
    
    	- (v0.1.0) Code that is specific to
    	itk::BinaryDilateImageFilter. Support for radius and foreground
    	value arguments. Structuring element is a ball.

---
+++ trunk/matlab/ItkToolbox/BinaryDilateFilter.cpp
@@ -0,0 +1,126 @@
+/*
+ * BinaryDilateFilter.cpp
+ *
+ * Code that is specific to itk::BinaryDilateImageFilter. Support for
+ * radius and foreground value arguments. Structuring element is a
+ * ball.
+ */
+
+ /*
+  * Author: Ramon Casero <rcasero@gmail.com>
+  * Copyright © 2011 University of Oxford
+  * Version: 0.0.1
+  * $Rev$
+  * $Date$
+  *
+  * University of Oxford means the Chancellor, Masters and Scholars of
+  * the University of Oxford, having an administrative office at
+  * Wellington Square, Oxford OX1 2JD, UK. 
+  *
+  * This file is part of Gerardus.
+  *
+  * This program is free software: you can redistribute it and/or modify
+  * it under the terms of the GNU General Public License as published by
+  * the Free Software Foundation, either version 3 of the License, or
+  * (at your option) any later version.
+  *
+  * This program is distributed in the hope that it will be useful,
+  * but WITHOUT ANY WARRANTY; without even the implied warranty of
+  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  * GNU General Public License for more details. The offer of this
+  * program under the terms of the License is subject to the License
+  * being interpreted in accordance with English Law and subject to any
+  * action against the University of Oxford being under the jurisdiction
+  * of the English Courts.
+  *
+  * You should have received a copy of the GNU General Public License
+  * along with this program.  If not, see
+  * <http://www.gnu.org/licenses/>.
+  */
+
+#ifndef BINARYDILATEFILTER_CPP
+#define BINARYDILATEFILTER_CPP
+
+/* C++ headers */
+#include <limits>
+
+/* Gerardus headers */
+#include "BinaryDilateFilter.hpp"
+
+/* 
+ * BinaryDilateFilter : BaseFilter
+ */
+template <class InVoxelType, class OutVoxelType>
+void BinaryDilateFilter<InVoxelType, 
+			OutVoxelType>::FilterSetup() {
+  
+  // check input parameters
+  if (this->nparam > 2) {
+    mexErrMsgTxt("Too many input arguments");
+  }
+  if (this->nparam > 0 && this->argParam == NULL) {
+    mexErrMsgTxt("Assertion fail: There is at least one parameter, but pointer to parameter array is NULL");
+  }
+  
+  // list of user-provided parameters for this filter (compulsory or
+  // optional)
+  unsigned long radius;   // (comp) radius of the ball in voxels
+  InVoxelType foreground; // (opt) voxels with this value will be
+                          // dilated. Default, maximum value of the
+                          // pixel type
+
+  // get parameters
+  if (this->nparam < 1) {
+    mexErrMsgTxt("Not enough input arguments");
+  }
+  if (this->nparam > 2) {
+    mexErrMsgTxt("Too many input arguments");
+  }
+  radius  = this->template 
+    getScalarParamValue<unsigned long>("RADIUS", 0, 0);
+  foreground = this->template 
+    getScalarParamValue<InVoxelType>("FOREGROUND", 1, std::numeric_limits<InVoxelType>::max());
+  
+  // create a local pointer to the filter so that we can use
+  // methods that are not part of the BaseFilter
+  typename FilterType::Pointer localFilter = 
+    dynamic_cast<typename BinaryDilateFilter<InVoxelType,
+			     OutVoxelType>::FilterType *>(this->filter.GetPointer());
+  
+  // instantiate structuring element
+  StructuringElementType structuringElement;
+  structuringElement.SetRadius(radius);
+  structuringElement.CreateStructuringElement();
+  localFilter->SetKernel(structuringElement);
+  
+  // pass parameters to filter
+  localFilter->SetForegroundValue(foreground);
+
+  // pass image to filter
+  localFilter->SetInput(this->image);
+  
+}
+
+/*
+ * Instantiate filter with all the input/output combinations that it
+ * accepts. This is necessary for the linker. The alternative is to
+ * have all the code in the header files, but this makes compilation
+ * slower and maybe the executable larger
+ */
+
+#define FILTERINST(T1, T2)						\
+  template class BinaryDilateFilter<T1, T2>;				\
+
+FILTERINST(mxLogical, mxLogical)
+FILTERINST(uint8_T, uint8_T)
+FILTERINST(int8_T, int8_T)
+FILTERINST(uint16_T, uint16_T)
+FILTERINST(int16_T, int16_T)
+FILTERINST(int32_T, int32_T)
+FILTERINST(int64_T, int64_T)
+FILTERINST(float, float)
+FILTERINST(double, double)
+
+#undef FILTERINST
+
+#endif /* BINARYDILATEFILTER_CPP */
