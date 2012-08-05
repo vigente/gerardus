19c2e37b490b8b7555984590c5341cd0ea5c2e62
rcasero <rcasero@e9491f86-7216-11de-921c-dbe4921c5040>
2012-08-05 22:11:05 +0000

    * Add matlab/ThirdPartyToolbox/MeshVoxelisation:
    	- Function to voxelise a closed (ie. watertight)
    	triangular-polygon mesh. By Adam H. Aitkenhead. Downloaded from
    	here
    
    	http://www.mathworks.com/matlabcentral/fileexchange/27390-mesh-voxelisation

---
+++ trunk/matlab/ThirdPartyToolbox/MeshVoxelisation/VOXELISE_example.m
@@ -0,0 +1,35 @@
+
+%Plot the original STL mesh:
+figure
+[stlcoords] = READ_stl('sample.stl');
+xco = squeeze( stlcoords(:,1,:) )';
+yco = squeeze( stlcoords(:,2,:) )';
+zco = squeeze( stlcoords(:,3,:) )';
+[hpat] = patch(xco,yco,zco,'b');
+axis equal
+
+%Voxelise the STL:
+[OUTPUTgrid] = VOXELISE(100,100,100,'sample.stl','xyz');
+
+%Show the voxelised result:
+figure;
+subplot(1,3,1);
+imagesc(squeeze(sum(OUTPUTgrid,1)));
+colormap(gray(256));
+xlabel('Z-direction');
+ylabel('Y-direction');
+axis equal tight
+
+subplot(1,3,2);
+imagesc(squeeze(sum(OUTPUTgrid,2)));
+colormap(gray(256));
+xlabel('Z-direction');
+ylabel('X-direction');
+axis equal tight
+
+subplot(1,3,3);
+imagesc(squeeze(sum(OUTPUTgrid,3)));
+colormap(gray(256));
+xlabel('Y-direction');
+ylabel('X-direction');
+axis equal tight
