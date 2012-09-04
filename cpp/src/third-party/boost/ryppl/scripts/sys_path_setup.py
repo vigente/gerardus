4f7de709173db9ffc92db3c581eaf5c22f833676
Ramón Casero <rcasero@gmail.com>
2012-09-04 11:52:35 +0000

    Delete boost code, as I want to delete the test and doc directories, but it has become complicated doing so with the current half-commit

--- cpp/src/third-party/boost/ryppl/scripts/sys_path_setup.py
+++
@@ -1,9 +0,0 @@
-import os, sys
-sys.path = [
-    os.path.join(
-        os.path.dirname(
-            os.path.dirname(
-                os.path.abspath(__file__)))
-        , 'ryppl'
-        , 'support')
-    ] + sys.path
