332e11d7ee91d64a7b8254421f8dc671a9d7c240
Ramón Casero <rcasero@users.noreply.github.com>
2012-09-05 00:27:22 +0000

    Add all missing header files, as even if a module is not used, it's code may be needed when including another

---
+++ include/boost/tti/gen/has_template_check_params_gen.hpp
@@ -0,0 +1,31 @@
+
+//  (C) Copyright Edward Diener 2011
+//  Use, modification and distribution are subject to the Boost Software License,
+//  Version 1.0. (See accompanying file LICENSE_1_0.txt or copy at
+//  http://www.boost.org/LICENSE_1_0.txt).
+
+#if !defined(TTI_TEMPLATE_PARAMS_GEN_HPP)
+#define TTI_TEMPLATE_PARAMS_GEN_HPP
+
+#include <boost/preprocessor/cat.hpp>
+
+/*
+
+  The succeeding comments in this file are in doxygen format.
+
+*/
+
+/** \file
+*/
+
+/// Generates the macro metafunction name for BOOST_TTI_HAS_TEMPLATE_CHECK_PARAMS.
+/**
+    name  = the name of the class template.
+
+    returns = the generated macro metafunction name.
+*/
+#define BOOST_TTI_HAS_TEMPLATE_CHECK_PARAMS_GEN(name) \
+  BOOST_PP_CAT(has_template_check_params_,name) \
+/**/
+
+#endif // TTI_TEMPLATE_PARAMS_GEN_HPP
