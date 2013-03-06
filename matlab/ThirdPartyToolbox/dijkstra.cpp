0d1454aecc8a0ee6e67331ce74a031a6ce6085f0
Ramón Casero <rcasero@gmail.com>
2013-03-06 19:25:20 +0000

    update the help header

--- matlab/ThirdPartyToolbox/dijkstra.cpp
+++ matlab/ThirdPartyToolbox/dijkstra.cpp
@@ -15,13 +15,25 @@
  *   entry in the sparse matrix is understood as a lack of edge (or edge
  *   weight = Inf).
  *
- *   S is a vector with a list of source node indices. A shortest path
- *   tree will be computed for each one of the nodes in S.
+ *   S is a vector with a list of source node indices. A shortest path tree
+ *   will be computed for each one of the nodes in S.
  *
  *   D, P are matrices where each row corresponds to a source node in S. D
- *   is the shortest distance from each node to the source node.
+ *   is the shortest distance from each target node to the source node.
  *
  *   P is the predecessor of each node in the shortest path tree.
+ *
+ * ... = DIJKSTRA(..., T)
+ *
+ *   T is a row matrix with a list of targets. For each source node S(i),
+ *   the algorithm will stop when it finds the shortest paths to all targets
+ *   in T. This is useful to save time when G is a large matrix, but we are
+ *   interested only in e.g. the distance between to nodes that are nearby.
+ *   By default, T is a list of all nodes.
+ *
+ *   Note that when running in this mode, D, P will still return values for
+ *   all target nodes, but you can only trust D, P values of nodes that are
+ *   on paths between the source and target nodes.
  *
  *   Because this function is implemented as a MEX function in C++,
  *   takes a sparse matrix at the input, and uses a Fibonacci heap
@@ -30,20 +42,19 @@
  *   [1]
  *   http://www.ahhf45.com/info/Data_Structures_and_Algorithms/resources/technical_artile/fibonacci_heap/fibonacci.htm
  *
- * Author: Mark Steyvers, Stanford University, 19 Dec 2000.
- * Version: 0.2.0
- * $Rev$
- * $Date$
+ * Original Author: Mark Steyvers, Stanford University, 19 Dec 2000.
  *
- * Modified by Ramon Casero <rcasero@gmail.com>, University of Oxford,
- * 23 Mar 2010 to also provide the predecessor list at the output.
- * 6  Feb 2013 to accept a list of targets. The algorithm will stop after hitting all targets
+ * Modified by Ramon Casero <rcasero@gmail.com>, University of Oxford,  23
+ * Mar 2010 to also provide the predecessor list at the output.
  *
  * This file is distributed as a derivative work of a third-party function
  * with project Gerardus.
  *
  * http://code.google.com/p/gerardus/
  *
+ * Version: 0.1.1
+ * $Rev$
+ * $Date$
  *
  **/
 
