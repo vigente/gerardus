b40cf6cc861b974dd10332b8c897fb1471caad7e
Ramón Casero <rcasero@users.noreply.github.com>
2013-06-05 10:34:09 +0000

    * Add cpp/src/third-party/CGAL-4.2:
    	- Code downloaded from
    	https://gforge.inria.fr/frs/?group_id=52&release_id=8001#cgal-_4.2-title-content.

---
+++ cpp/src/third-party/CGAL-4.2/include/CGAL/Algebraic_kernel_rs_gmpq_d_1.h
@@ -0,0 +1,31 @@
+// Copyright (c) 2006-2010 Inria Lorraine (France). All rights reserved.
+//
+// This file is part of CGAL (www.cgal.org); you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public License as
+// published by the Free Software Foundation; either version 3 of the License,
+// or (at your option) any later version.
+//
+// Licensees holding a valid commercial license may use this file in
+// accordance with the commercial license agreement provided with the software.
+//
+// This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
+// WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+//
+// $URL$
+// $Id$
+//
+// Author: Luis Peñaranda <luis.penaranda@gmx.com>
+
+#ifndef CGAL_ALGEBRAIC_KERNEL_RS_GMPQ_1
+#define CGAL_ALGEBRAIC_KERNEL_RS_GMPQ_1
+
+#include <CGAL/Gmpq.h>
+#include <CGAL/RS/Algebraic_kernel_rs_1.h>
+
+namespace CGAL{
+
+typedef Algebraic_kernel_rs_1<Gmpq>     Algebraic_kernel_rs_gmpq_d_1;
+
+}
+
+#endif  // CGAL_ALGEBRAIC_KERNEL_RS_GMPQ_1
---
+++ cpp/src/third-party/CGAL-4.2/include/CGAL/version.h
@@ -0,0 +1,35 @@
+// Copyright (c) 2006  Utrecht University (The Netherlands),
+// ETH Zurich (Switzerland),
+// INRIA Sophia-Antipolis (France),
+// Max-Planck-Institute Saarbruecken (Germany),
+// and Tel-Aviv University (Israel).  All rights reserved.
+//
+// This file is part of CGAL (www.cgal.org); you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public License as
+// published by the Free Software Foundation; either version 3 of the License,
+// or (at your option) any later version.
+//
+// Licensees holding a valid commercial license may use this file in
+// accordance with the commercial license agreement provided with the software.
+//
+// This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
+// WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+//
+// Author(s)     : -
+
+// This file is automatically created by create_internal_release.
+// Do not edit manually.
+
+#ifndef CGAL_VERSION_H
+#define CGAL_VERSION_H
+
+#define CGAL_xstr(s) #s
+#define CGAL_str(s) CGAL_xstr(s)
+
+#define CGAL_VERSION 4.2
+#define CGAL_VERSION_NR 1040201000
+#define CGAL_SVN_REVISION 99999
+#define CGAL_GIT_HASH 492c8442eb9ddd696566187fade6be3a3aa58647
+#define CGAL_VERSION_STR CGAL_str(CGAL_VERSION)
+
+#endif
