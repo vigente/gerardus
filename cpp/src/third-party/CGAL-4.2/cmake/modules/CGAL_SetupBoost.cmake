ce16aa61583e73622a72edbae728166455bf1728
Ramón Casero <rcasero@users.noreply.github.com>
2013-06-05 16:41:47 +0000

    * cpp/src/third-party/CGAL-4.2/cmake/modules/CGAL_SetupBoost.cmake: (0.1.0)
    	- We distribute the boost libraries with gerardus, so we don't
    	need to look for them as an external package. In fact, if we don't
    	comment out the find_package() below, we get a recursive loop with
    	CGAL that throws a configuration warning.
    
    	* cpp/src/third-party/CGAL-4.2/CMakeLists.txt: (0.1.0)
    
    	- Modifications to make CGAL build as a subproject in Gerardus.
    
    	* cpp/src/CMakeLists.txt: (0.2.4)
    
    	- Now instead of giving an error for the Windows build, we are
    	going to use the pre-compiled GMP and MPFR libraries distributed
    	with the Windows installer for CGAL. For Unix-like builds, we
    	build GMP and MPFR from the source code, as we were doing
    	before. This hasn't been tested with Windows yet, though.
    
    	- We need to make sure that the boost libraries are going to be
    	built as dynamic libraries. Without the following directive, they
    	get built as static, and then CGAL gives an error when it tries to
    	build a dynamic library with them.

--- cpp/src/third-party/CGAL-4.2/cmake/modules/CGAL_SetupBoost.cmake
+++ cpp/src/third-party/CGAL-4.2/cmake/modules/CGAL_SetupBoost.cmake
@@ -1,13 +1,25 @@
+# Modified by Ramon Casero for project Gerardus (see comments headed
+# by "gerardus:").
+#
+# Version: 0.1.0
+# $Rev$
+# $Date$
+
 if ( NOT CGAL_Boost_Setup )
 
-  include(CGAL_TweakFindBoost)
-  # In the documentation, we say we require Boost-1.39, but technically we
-  # require 1.33.1. Some packages may require more recent versions, though.
-  find_package( Boost 1.33.1 REQUIRED thread system )
+  # gerardus: We distribute the boost libraries with gerardus, so we
+  # don't need to look for them as an external package. In fact, if we
+  # don't comment out the find_package() below, we get a recursive
+  # loop with CGAL that throws a configuration warning
+
+  # include(CGAL_TweakFindBoost)
+  # # In the documentation, we say we require Boost-1.39, but technically we
+  # # require 1.33.1. Some packages may require more recent versions, though.
+  # find_package( Boost 1.33.1 REQUIRED thread system )
   
-  message( STATUS "Boost include:     ${Boost_INCLUDE_DIRS}" )
-  message( STATUS "Boost libraries:   ${Boost_LIBRARIES}" )
-  message( STATUS "Boost definitions: ${Boost_DEFINITIONS}" )
+  # message( STATUS "Boost include:     ${Boost_INCLUDE_DIRS}" )
+  # message( STATUS "Boost libraries:   ${Boost_LIBRARIES}" )
+  # message( STATUS "Boost definitions: ${Boost_DEFINITIONS}" )
   
   set ( CGAL_USE_BOOST 1 )
   
