5bd0600befb347d7af9119d867a1356e64ececcd
Ramón Casero <rcasero@gmail.com>
2013-06-11 23:15:36 +0000

    * Delete cpp/src/third-party/boost-1.50.0:

--- cpp/src/third-party/boost-1.50.0/ryppl/ryppl/commands/__init__.py
+++
@@ -1,8 +0,0 @@
-# Copyright Dave Abrahams 2012. Distributed under the Boost
-# Software License, Version 1.0. (See accompanying
-# file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
-import glob, os
-_pyfiles = glob.glob(os.path.join(os.path.dirname(__file__),'*.py*'))
-_modules = set(os.path.splitext(os.path.basename(x))[0] for x in _pyfiles)
-__all__ = [x for x in _modules if x != '__init__']
-
