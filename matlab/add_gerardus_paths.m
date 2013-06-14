e27cb645541c70e7b23663bd78d3f286df9b20d8
Ramón Casero <rcasero@gmail.com>
2013-06-14 16:31:12 +0000

    * Add matlab/add_gerardus_paths.m: (0.1.0)
    	- Add all Gerardus toolboxes to the Matlab path.

---
+++ matlab/add_gerardus_paths.m
@@ -0,0 +1,85 @@
+% ADD_GERARDUS_PATHS  Add all Gerardus toolboxes to the Matlab path
+%
+%   add_gerardus_paths
+%
+%   This script takes no input arguments and produces no output arguments.
+%   It will check the current Matlab path, and add to it those Gerardus
+%   toolboxes that are not already in the path.
+%
+%   It then asks the user whether the updated path should be saved to
+%   gerardus/matlab/pathdef.m.
+
+% Author: Ramon Casero <rcasero@gmail.com>
+% Copyright © 2013 University of Oxford
+% Version: 0.1.0
+% $Rev$
+% $Date$
+%
+% University of Oxford means the Chancellor, Masters and Scholars of
+% the University of Oxford, having an administrative office at
+% Wellington Square, Oxford OX1 2JD, UK. 
+%
+% This file is part of Gerardus.
+%
+% This program is free software: you can redistribute it and/or modify
+% it under the terms of the GNU General Public License as published by
+% the Free Software Foundation, either version 3 of the License, or
+% (at your option) any later version.
+%
+% This program is distributed in the hope that it will be useful,
+% but WITHOUT ANY WARRANTY; without even the implied warranty of
+% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+% GNU General Public License for more details. The offer of this
+% program under the terms of the License is subject to the License
+% being interpreted in accordance with English Law and subject to any
+% action against the University of Oxford being under the jurisdiction
+% of the English Courts.
+%
+% You should have received a copy of the GNU General Public License
+% along with this program.  If not, see
+% <http://www.gnu.org/licenses/>.
+
+% check arguments
+narginchk(0, 0);
+nargoutchk(0, 0);
+
+% path to the gerardus/matlab directory (or equivalently, path to this file
+% add_gerardus_paths.m)
+matdirpath = mfilename('fullpath');
+matdirpath = fileparts(matdirpath);
+
+% list of toolboxes to add to the path
+toolboxes = { ...
+    'CardiacToolbox', ...
+    'FileFormatToolbox', ...
+    'FiltersToolbox', ...
+    'ItkToolbox', ...
+    'ManifoldToolbox', ...
+    'PointsToolbox', ...
+    'ThirdPartyToolbox', ...
+    ['ThirdPartyToolbox' filesep 'niftiToolbox'], ...
+    ['ThirdPartyToolbox' filesep 'sphsplineToolbox'], ...
+    ['ThirdPartyToolbox' filesep 'TiffreadToolbox'] ...
+    };
+
+% current Matlab paths
+pth = path;
+
+for I = 1:length(toolboxes)
+    
+    % if the toolbox is not in the path already, add it
+    if isempty(strfind(pth, [matdirpath filesep toolboxes{I}]))
+        disp(['Adding ' toolboxes{I} ' to the path'])
+        
+        addpath([matdirpath filesep toolboxes{I}]);
+    else
+        disp(['Skipping ' toolboxes{I} '. Already in the path'])
+    end
+    
+end
+
+% ask user whether to save the new path
+str = input('Save current path to gerardus/matlab (y/N)? ', 's');
+if (str == 'y')
+    savepath([matdirpath filesep 'pathdef.m'])
+end
