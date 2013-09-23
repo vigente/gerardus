fd9fe21cb9d11e06f79ac2be1a0637a8fd108198
Ramón Casero <rcasero@gmail.com>
2013-09-23 20:54:48 +0000

    * matlab/MatlabImageHeader.h: (0.2.0) 	* Delete matlab/MatlabImageHeader.cxx: 	* matlab/ItkToolbox/CMakeLists.txt: (0.6.6) 	* matlab/CgalToolbox/CMakeLists.txt: (0.2.13)
    	- Combine MatlabImageHeader.cxx and MatlabImageHeader.h into
    	MatlabImageHeader.h. This simplifies it when we have to use this
    	class into other programs.

--- matlab/MatlabImageHeader.h
+++ matlab/MatlabImageHeader.h
@@ -10,7 +10,7 @@
  /*
   * Author: Ramon Casero <rcasero@gmail.com>
   * Copyright © 2012-2013 University of Oxford
-  * Version: 0.1.1
+  * Version: 0.2.0
   * $Rev$
   * $Date$
   *
@@ -42,6 +42,14 @@
 #ifndef MATLABIMAGEHEADER_H
 #define MATLABIMAGEHEADER_H
 
+/* mex headers */
+#include <mex.h>
+
+/* C++ headers */
+#include <iostream>
+#include <vector>
+#include <string>
+
 class MatlabImageHeader {
 
  public:
@@ -59,5 +67,120 @@ class MatlabImageHeader {
   }
   
 };
+
+// constructor of the auxiliary class that preprocesses a Matlab
+// argument that corresponds to an image
+MatlabImageHeader::MatlabImageHeader(const mxArray *arg, std::string paramName) {
+
+  if (arg == NULL) {
+    mexErrMsgTxt(("Parameter " + paramName + " is a NULL pointer").c_str());
+  }
+
+  // get pointer to image data
+  if (mxIsEmpty(arg)) { // input is empty
+
+    this->data = NULL;
+    this->type = mxUNKNOWN_CLASS;
+    this->size.resize(0);
+    this->spacing.resize(0);
+    this->origin.resize(0);
+    return;
+
+  } else if (mxIsStruct(arg)) { // input is a SCI MAT struct
+    // read struct fields...
+
+    // ... data field (where the image is contained)
+    this->data = mxGetField(arg, 0, "data");
+    if (this->data == NULL) {
+      mexErrMsgTxt(("Parameter " + paramName + 
+		    ": Struct format error: Missing or invalid data field").c_str());
+    }
+  } else { // input is not a struct, but just an image array
+
+    this->data = const_cast<mxArray *>(arg);
+
+  }
+
+  // get image type
+  this->type = mxGetClassID(data);
+
+  mwSize ndim; // number of dimensions in the image
+  mwSize *dims; // dimensions array
+
+  // get number of dimensions in the input image
+  if (!mxIsNumeric(data) 
+      && !mxIsLogical(data)) {
+    mexErrMsgTxt(("Parameter " + paramName + 
+		  " must be a numeric or boolean array").c_str());
+  }
+  ndim = mxGetNumberOfDimensions(data);
+  
+  // get image size
+  dims = const_cast<mwSize *>(mxGetDimensions(data));
+  if (dims == NULL) {
+    mexErrMsgTxt(("Parameter " + paramName + 
+		  ": Matlab cannot obtain vector of dimensions from the data").c_str());
+  }
+  for (unsigned int i = 0; i < ndim; ++i) {
+    this->size.push_back(dims[i]);
+  }
+
+  // get rest of image info (spacing, origin)
+  if (mxIsStruct(arg)) { // input is a SCI MAT struct{
+    // ... axis field
+    mxArray *axis = mxGetField(arg, 0, "axis");
+    if (axis == NULL) {
+      mexErrMsgTxt(("Parameter " + paramName + 
+  		    ": Struct format error: Missing or invalid axis field").c_str());
+    }
+    if (!mxIsStruct(axis)) {
+      mexErrMsgTxt(("Parameter " + paramName + 
+  		    ": Struct format error: axis field is not a struct").c_str());
+    }
+    if (mxGetM(axis) < ndim) {
+      mexErrMsgTxt(("Parameter " + paramName + 
+  		    ": Struct format error: not enough elements in the axis field vector").c_str());
+    }
+
+    // ... spacing field (image resolution)
+    // ... min field (note: "min"    = left corner of first voxel,
+    //                      "origin" = centre of first voxel)
+    mxArray *spacingMx, *minMx;
+    double *spacingMxp, *minMxp;
+    for (unsigned int i = 0; i < ndim; ++i) {
+      spacingMx = mxGetField(axis, i, "spacing");
+      if (spacingMx == NULL) {
+  	mexErrMsgTxt(("Parameter " + paramName + 
+  		      ": Struct format error: Missing or invalid axis.spacing field").c_str());
+      }
+      minMx = mxGetField(axis, i, "min");
+      if (minMx == NULL) {
+  	mexErrMsgTxt(("Parameter " + paramName + 
+  		      ": Struct format error: Missing or invalid axis.min field").c_str());
+      }
+      spacingMxp = (double *)mxGetData(spacingMx);
+      minMxp = (double *)mxGetData(minMx);
+      if (mxIsFinite(spacingMxp[0]) && !mxIsNaN(spacingMxp[0])) {
+  	spacing.push_back(spacingMxp[0]);
+      } else {
+  	spacing.push_back(1.0);
+      }
+      if (mxIsFinite(minMxp[0]) && !mxIsNaN(minMxp[0])) {
+  	origin.push_back(minMxp[0] + spacing[i] / 2.0);
+      } else {
+  	origin.push_back(-spacing[i] / 2.0);
+      }
+    }
+
+  } else { // input is not a struct, but just an image array
+
+    for (unsigned int i = 0; i < ndim; ++i) {
+      spacing.push_back(1.0);
+      origin.push_back(0.0);
+    }
+
+  }
+
+}
 
 #endif /* MATLABIMAGEHEADER_H */
