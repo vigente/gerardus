ea900913c67049747ff32cc27b758f9badc1d543
Ramón Casero <rcasero@gmail.com>
2013-10-09 16:04:11 +0000

    * matlab/CgalToolbox/CgalMeshSegmentation.cpp: (0.1.4) 	* matlab/CgalToolbox/cgal_meshseg.m: (0.1.1)
    	- Add warning to the help header about this function producing
    	stray vertices, non-oriented triangles and holes, and how to
    	correct it within Gerardus.

--- matlab/CgalToolbox/cgal_meshseg.m
+++ matlab/CgalToolbox/cgal_meshseg.m
@@ -1,6 +1,6 @@
 function varargout = cgal_meshseg(varargin)
 % CGAL_MESHSEG  Surface meshing of an isosurface from a segmentation or
-% grayscale image
+% grayscale image.
 %
 % This function is a Matlab wrapper of the CGAL 3D Surface Mesh Generation
 % for a grayscale input image.
@@ -76,10 +76,23 @@ function varargout = cgal_meshseg(varargin)
 %     MANIFOLD=true, the mesher uses CGAL::Manifold_tag: "When instantiated
 %     with the tag Manifold_tag the function template make_surface_mesh
 %     ensures that the output mesh is a manifold surface without boundary".
+%
+% Important!
+%
+% Note that this function can produce meshes with (1) stray vertices that
+% belong to no triangle, (2) triangles not oriented with respect to the
+% manifold, and (3) holes in the surface. These problems can be solved with
+% the following functions available in Gerardus:
+%
+%   [tri, x] = tri_squeeze(tri, x); % remove stray vertices
+%   [x, tri] = meshcheckrepair(x, tri, 'deep'); % correct orientation
+%   tri = cgal_tri_fillholes(tri, x); % fill holes in the surface
+%
+% See also: bwmesh, tri_squeeze, meshcheckrepair, cgal_tri_fillholes.
 
 % Author: Ramon Casero <rcasero@gmail.com>
 % Copyright © 2013 University of Oxford
-% Version: 0.1.0
+% Version: 0.1.1
 % $Rev$
 % $Date$
 %
