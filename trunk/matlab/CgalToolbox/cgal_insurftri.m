fc4cb5d34a42d9d8da947063bbabbcf0cf5f3689
rcasero <rcasero@e9491f86-7216-11de-921c-dbe4921c5040>
2013-10-09 15:46:07 +0000

    minor

--- trunk/matlab/CgalToolbox/cgal_insurftri.m
+++ trunk/matlab/CgalToolbox/cgal_insurftri.m
@@ -14,7 +14,7 @@ function varargout = cgal_insurftri(varargin)
 %
 % ISIN = cgal_insurftri(TRI, X, XI)
 %
-%   TRI is a 3-column matrix. Each row represents the indices of the tree
+%   TRI is a 3-column matrix. Each row represents the indices of the three
 %   vertices that form a triangle. TRI as a whole represents the closed
 %   surface.
 %
