0e3b44327d5fd160689e91a14d6a6483ae8e5bd2
rcasero <rcasero@e9491f86-7216-11de-921c-dbe4921c5040>
2013-10-25 11:45:44 +0000

    * matlab/PointsToolbox/surface_param.m: (0.4.0) 	* matlab/test/test_surface_param.m: (0.4.0)
    	- Rewrite of methods 'isomap' and 'cmdsmap'. The first one now has
    	an input syntax congruent with the method. The second one is the
    	open surface classic MDS, that is, only valid for full distance
    	matrices, using classic MDS to solve the parameterization.
    
    	- Add 'lmdscale' for open surface with local neighbourhoods. But
    	still needs correcting.

--- trunk/matlab/PointsToolbox/surface_param.m
+++ trunk/matlab/PointsToolbox/surface_param.m
@@ -40,109 +40,160 @@ function [uv, out] = surface_param(x, param)
 %
 %     Parametrizations details:
 %
-%     * 'xy' (default): Projection on the xy-plane, i.e. xy-coordinates of
-%       the points in X.
+%   * 'xy' (default): Projection on the xy-plane, i.e. xy-coordinates of
+%     the points in X.
 %
-%     * 'pca': Projection on the plane defined by the two eigenvectors with
-%       the largest eigenvalues.
+%   * 'pca': Projection on the plane defined by the two eigenvectors with
+%     the largest eigenvalues.
 %
-%     * 'isomap': Isometric mapping (Tenenbaum et al. [1][2]). This is a
-%       non-linear method that can be applied to complex open surfaces that
-%       bend on themselves or are rolled-up. This option uses the
-%       third-party function IsomapII.
+%   * 'isomap': Isometric mapping (Tenenbaum et al. [1][2]). This is a
+%     non-linear method that can be applied to complex open surfaces that
+%     bend on themselves or are rolled-up. This option uses the
+%     third-party function IsomapII. Because isomap uses Dijkstra's
+%     shortest-path algorithm internally, it suffers strongly from
+%     metrication errors in regular meshes. On the other hand, it can be
+%     applied to any adjacency graph, not only meshes.
 %
-%       PARAM.d:     [Opt] Distance matrix between points. By default,
-%                    PARAM.d is the Euclidean matrix of distances between
-%                    every pair of points.
+%     Small neighbourhoods capture higher curvature in the surface, but
+%     too small neighbourhoods create several connected components and
+%     will produce an error.
 %
-%                    Providing the matrix allows the user to define
-%                    specific neighbourhoods and topologies, or even use
-%                    non-Euclidean distances.
+%     PARAM.d: [Opt] Distance matrix between points. The parameter can have
+%       the following formats:
+%       (1) a full N x N matrix with distances between all pairs of points.
+%       In this case, you can define the local neighbourhoods using
+%       PARAM.neigh and PARAM.size, so that some of the connections in the
+%       full matrix will be deleted and replaced by Disjkstra's
+%       shortest-path lengths.
+%       (2) a sparse N x N matrix (missing entries are treated as Inf). In
+%       this case, you usually want to make PARAM.size=Inf, because the
+%       local neighbourhood is already given by the sparse matrix. The
+%       missing connections will be replaced internally by Disjkstra's
+%       shortest-path lengths.
+%       (3) the name of a function (e.g. 'd_fun') that takes one argument,
+%       i, and returns a row vector containing the distances from all N
+%       points to point i.
 %
-%       PARAM.neigh: [Opt] Type of neighbourhood ('epsilon' or 'k'). 
-%       PARAM.size:  [Opt] Size of neighbourhood.
+%       If not provided, PARAM.d is the full Euclidean matrix of distances
+%       between every pair of points.
 %
-%                    Each point in X is directly connected only to
-%                    neighbours within a distance PARAM.size (for
-%                    'epsilon'), or the PARAM.size closest neighbours (for
-%                    'k'), according to the distances in PARAM.d. This
-%                    allows to further restrict the neighbourhoods in
-%                    PARAM.d. Note that in the 'k' case, in order to obtain
-%                    a symmetric local neighbourhood distance matrix, the
-%                    number of nearest neighbours may be increased.
+%       As a particular case, note that in case of a triangular mesh, the
+%       sparse local distance that corresponds to the mesh can be computed
+%       using dmatrix_mesh().
 %
-%                    Small neighbourhoods capture higher curvature in the
-%                    surface, but too small neighbourhoods create several
-%                    connected components and will produce an error.
+%     PARAM.neigh: [Opt] Type of neighbourhood ('epsilon' or 'k'). 
+%     PARAM.size:  [Opt] Size of neighbourhood.
 %
-%       PARAM.options: [Opt] Extra arguments passed to IsomapII. Most users
-%                    won't need to change this. For details, see
-%                    help IsomapII.
+%       Each point in X is directly connected only to neighbours within a
+%       distance PARAM.size (for 'epsilon'), or the PARAM.size closest
+%       neighbours (for 'k'), according to the distances in PARAM.d. This
+%       allows to further restrict the neighbourhoods in PARAM.d.
 %
-%     * 'sphproj':   Direct projection on unit sphere centered on point set
-%       centroid.
+%     PARAM.options: [Opt] Extra arguments passed to IsomapII. Most users
+%       won't need to change this. For details, see help to IsomapII().
 %
-%       OUT.rmed:    Median value of the radii of all points in the point
+%   * 'cmdsmap': Open surface classical MDS mapping improved with a Fast
+%     Marching method (Zigelman et al. [3]). The fast marching algorithm
+%     doesn't suffer from metrication errors on regular meshes as badly as
+%     Dijkstra's. On the other hand, the current implementation is limited
+%     to triangular meshes, while Dijkstra's will work with any adjacency
+%     graph. This option uses classical MDS on a full distance matrix, so
+%     it's not valid for local neighbourhoods or closed surfaces. For local
+%     neighbourhoods, see 'lmdscale' below. For closed surfaces, see 
+%
+%     PARAM.d or PARAM.tri must be provided. If PARAM.d is provided,
+%     PARAM.tri and PARAM.options are ignored. Otherwise, PARAM.d is
+%     computed from PARAM.tri and PARAM.options.
+%
+%     PARAM.d: [Req/Opt] This is expected to be the full distance matrix
+%       between mesh vertices that can be computed with
+%         [~, param.d] = dmatrix_mesh(param.tri, x, 'fastmarching');
+%       Note that for comparison purposes, Dijkstra's algorithm can be used
+%       instead of Fast Marching
+%         [~, param.d] = dmatrix_mesh(param.tri, x, 'dijkstra');
+%       The latter is equivalent to the 'isomap' method above.
+%
+%     PARAM.tri: [Req/Opt] 3-column matrix. Each row contains the 3 nodes
+%       that form one triangular facet in the mesh.
+%
+%     PARAM.options: [Opt] Extra arguments passed to the fast marching
+%       method:
+%       - options.constraint_map: Exploration radius to reduce the set of
+%         explored points. Only points with current distance smaller than L
+%         will be expanded. Set some entries of L to -Inf to avoid any
+%         exploration of these points.
+%       - options.W: non-uniform speed.
+%       - options.end_points: stop when these points are reached.
+%       - options.nb_iter_max: stop when a given number of iterations is
+%         reached.
+%       - options.heuristic: heuristic that tries to guess the distance
+%         that remains from a given node to a given target. This is an
+%         array of same size as W.
+%
+%
+%   * 'sphproj':   Direct projection on unit sphere centered on point set
+%     centroid.
+%
+%     OUT.rmed:    Median value of the radii of all points in the point
 %                    set.
 %
-%     * 'cald':      Li Shen's Control Area and Length Distortions (CALD)
-%       spherical parametrization [3].
+%   * 'cald':      Li Shen's Control Area and Length Distortions (CALD)
+%     spherical parametrization [4][5].
 %
-%       PARAM.MeshGridSize: The interpolation mesh used to smooth the
-%                    spherical parametrization has length
-%                    2*PARAM.MeshGridSize+1. By default,
-%                    PARAM.MeshGridSize = 50.
+%     PARAM.MeshGridSize: The interpolation mesh used to smooth the
+%                  spherical parametrization has length
+%                  2*PARAM.MeshGridSize+1. By default,
+%                  PARAM.MeshGridSize = 50.
 %
-%       PARAM.MaxSPHARMDegree: Degree of the spherical harmonics used for
-%                   interpolation to smooth the spherical parametrization.
-%                   By default, PARAM.MaxSPHARMDegree = 6.
+%     PARAM.MaxSPHARMDegree: Degree of the spherical harmonics used for
+%                 interpolation to smooth the spherical parametrization.
+%                 By default, PARAM.MaxSPHARMDegree = 6.
 %
-%       PARAM.Tolerance: Scalar. In the interpolation smoothing, grid
-%                   points with a height larger than PARAM.Tolerance*gmin,
-%                   where gmin is the lowest point in the grid, will be
-%                   truncated. By default, PARAM.Tolerance = 2.
+%     PARAM.Tolerance: Scalar. In the interpolation smoothing, grid
+%                 points with a height larger than PARAM.Tolerance*gmin,
+%                 where gmin is the lowest point in the grid, will be
+%                 truncated. By default, PARAM.Tolerance = 2.
 %
-%       PARAM.Smoothing: Scalar. Before parametrization smoothing,
-%                  parametrization triangle areas relative to triangular
-%                  mesh areas are smoothed by elevating to
-%                  (1/PARAM.Smoothing). By default, PARAM.Smoothing = 2.
+%     PARAM.Smoothing: Scalar. Before parametrization smoothing,
+%                parametrization triangle areas relative to triangular
+%                mesh areas are smoothed by elevating to
+%                (1/PARAM.Smoothing). By default, PARAM.Smoothing = 2.
 %
-%       PARAM.Iteration: Maximum number of smoothing iterations. Smoothing
-%                  will stop before reaching PARAM.Iteration if the
-%                  algorithm converges to a solution. By default,
-%                  PARAM.Iteration = 100.
+%     PARAM.Iteration: Maximum number of smoothing iterations. Smoothing
+%                will stop before reaching PARAM.Iteration if the
+%                algorithm converges to a solution. By default,
+%                PARAM.Iteration = 100.
 %
-%       PARAM.LocalIteration: Number of smoothing iteration in the local
-%                 smoothing algorithm. By default, PARAM.LocalIteration =
-%                 10.
+%     PARAM.LocalIteration: Number of smoothing iteration in the local
+%               smoothing algorithm. By default, PARAM.LocalIteration = 10.
 %
-%     * 'sphisomap': Our extension to the Isomap method for closed
-%       surfaces. The result is a parametrization on the unit sphere.
+%   * 'sphisomap': Our extension to the Isomap method for closed
+%     surfaces. The result is a parametrization on the unit sphere.
 %
-%       PARAM.d:     See 'isomap' above.
-%       PARAM.neigh: See 'isomap' above.
-%       PARAM.size:  See 'isomap' above.
+%     PARAM.d:     See 'isomap' above.
+%     PARAM.neigh: See 'isomap' above.
+%     PARAM.size:  See 'isomap' above.
 %
-%       PARAM.init:   Initial parametrization. It can be a 2-column matrix,
-%                     in which case those are the coordinates directly
-%                     used, or a string:
+%     PARAM.init:   Initial parametrization. It can be a 2-column matrix,
+%                   in which case those are the coordinates directly
+%                   used, or a string:
 %
-%                     'sphproj': (default) Surface points are projected
-%                     onto the sphere along the radii to their centroid.
+%                   'sphproj': (default) Surface points are projected
+%                   onto the sphere along the radii to their centroid.
 %
-%                     'random': Uniformly random distribution of points on
-%                     the sphere. Counter-intuitively, this can work better
-%                     than sphproj, because it may prevent MDS from getting
-%                     trapped in local minima.
+%                   'random': Uniformly random distribution of points on
+%                   the sphere. Counter-intuitively, this can work better
+%                   than sphproj, because it may prevent MDS from getting
+%                   trapped in local minima.
 %
-%        PARAM.*:     Any other fields are passed to function
-%                     smdscale(...,OPT=PARAM). This allows to pass e.g.
-%                     stopping conditions to the spherical MDS algorithm.
-%                     See "help smdscale" for all options.
+%      PARAM.*:     Any other fields are passed to function
+%                   smdscale(...,OPT=PARAM). This allows to pass e.g.
+%                   stopping conditions to the spherical MDS algorithm.
+%                   See "help smdscale" for all options.
 %
-%        OUT.err:     Frobenius norm of the distance matrix error
-%                     D_xy - D_uv at each step of the optimisation
-%                     algorithm (moving each point counts as a step).
+%      OUT.err:     Frobenius norm of the distance matrix error
+%                   D_xy - D_uv at each step of the optimisation
+%                   algorithm (moving each point counts as a step).
 %
 %
 %
@@ -152,16 +203,21 @@ function [uv, out] = surface_param(x, param)
 %
 % [2] Isomap Homepage, http://isomap.stanford.edu/
 %
-% [3] Shen and Makedon, "Spherical mapping for processing of 3D closed
+% [3] G. Zigelman, R. Kimmel, and N. Kiryati, "Texture mapping using
+% surface flattening via multidimensional scaling,” Visualization and
+% Computer Graphics, IEEE Transactions on, vol. 8, no. 2, pp. 198–207,
+% 2002.
+%
+% [4] Shen and Makedon, "Spherical mapping for processing of 3D closed
 % surfaces", Image and Vision Computing, 24(7):743–761, 2006.
 % http://www.sciencedirect.com/science/article/pii/S0262885606000485
 %
-% [4] CALD and SPHARM-MAT homepage,
+% [5] CALD and SPHARM-MAT homepage,
 % http://www.iupui.edu/~shenlab/software.html
 
 % Author: Ramon Casero <rcasero@gmail.com>
 % Copyright © 2013 University of Oxford
-% Version: 0.3.0
+% Version: 0.4.0
 % $Rev$
 % $Date$
 % 
@@ -228,26 +284,142 @@ switch param.type
         uv = uv(:, 1:2);
         
     case 'isomap'
+        
+        % IsomapII takes the following distance matrices:
+        %  (1) a full N x N matrix (as in isomap.m)  
+        %  (2) a sparse N x N matrix (missing entries are treated as INF)
+        %  (3) the name of a function (e.g. 'd_fun') that takes
+        %        one argument, i, and returns a row vector containing the 
+        %        distances from all N points to point i.        
+        
+        % default parameters
+        if (~isfield(param, 'neigh') || isempty(param.neigh))
+            param.neigh = 'epsilon';
+        end
+        if (~isfield(param, 'size') || isempty(param.size))
+            param.size = Inf;
+        end
 
         % if distance matrix is not provided by the user, it is computed
-        % internally as the full Euclidean distance matrix. Note that we
-        % can still define local neighbourdhoods with the size parameter
+        % internally as the full Euclidean distance matrix
         if (~isfield(param, 'd') || isempty(param.d))
             param.d = dmatrix(x', x', 'euclidean');
         end
         
         % compute 2-d projection of the 3-d data
-        options.dims = 2;
-        options.display = 0;
-        options.overlay = 0;
-        options.verbose = 0;
-        options.dijkstra = 1;
-        uv = IsomapII(param.d, param.neigh, param.size, options);
+        if (~isfield(param, 'options') || isempty(param.options))
+            param.options.dims = 2;
+            param.options.display = 0;
+            param.options.overlay = 0;
+            param.options.verbose = 0;
+            param.options.dijkstra = 1;
+        end
+        uv = IsomapII(param.d, param.neigh, param.size, param.options);
         uv = uv.coords{1}';
         if (size(uv, 1) ~= size(x, 1))
             error('The neighbourhood size (param.size) is too small to connect all points')
         end
         
+    case 'cmdsmap'
+        
+        % if distance matrix is not provided
+        if (~isfield(param, 'd') || isempty(param.d))
+            
+            % the user hasn't provided the mesh triangulation either
+            if (~isfield(param, 'tri') || isempty(param.tri))
+                error('Either PARAM.d or PARAM.tri must be provided')
+            end
+            
+            % default for fast marching method options
+            if (~isfield(param, 'options') || isempty(param.options))
+                param.options = [];
+            end
+
+            % remove fast marching options that don't apply to this method
+            if (isfield(param.options, 'constraint_map'))
+                warning('Gerardus:InvalidInputArg', 'PARAM.options.constraint_map ignored, this method only works with a full distance matrix')
+                param.options = rmfield(param.options, 'constraint_map');
+            end
+            if (isfield(param.options, 'end_points'))
+                warning('Gerardus:InvalidInputArg', 'PARAM.options.end_points ignored, this method only works with a full distance matrix')
+                param.options = rmfield(param.options, 'end_points');
+            end
+            if (isfield(param.options, 'nb_iter_max'))
+                warning('Gerardus:InvalidInputArg', 'PARAM.options.nb_iter_max ignored, this method only works with a full distance matrix')
+                param.options = rmfield(param.options, 'nb_iter_max');
+            end
+            
+            % compute full distance matrix from the mesh
+            [~, param.d] = dmatrix_mesh(param.tri, x, 'fastmarching', param.options);
+            
+        end
+        
+        % if the user provided a sparse distance matrix, we assume that
+        % it's a mistake
+        if (issparse(param.d))
+            error('PARAM.d must be a full distance matrix, not a sparse mesh adjacency-distance matrix');
+        end
+        
+        % use classic MDS for the optimization
+        uv = cmdscale(param.d);
+        
+        % the output parametrization is 2D
+        uv = uv(:, 1:2);
+            
+    case 'lmdscale'
+        
+        % extract parameters for the fast marching algorithm
+        options = [];
+        if (~isfield(param, 'W') || isempty(param.W))
+            % do nothing
+        else
+            options.W = param.W;
+        end
+        if (~isfield(param, 'end_points') || isempty(param.end_points))
+            % do nothing
+        else
+            options.end_points = param.end_points;
+        end
+        if (~isfield(param, 'nb_iter_max') || isempty(param.nb_iter_max))
+            % do nothing
+        else
+            options.nb_iter_max = param.nb_iter_max;
+        end
+        if (~isfield(param, 'heuristic') || isempty(param.heuristic))
+            % do nothing
+        else
+            options.heuristic = param.heuristic;
+        end
+        if (~isfield(param, 'constraint_map') || isempty(param.constraint_map))
+            % do nothing
+        else
+            options.constraint_map = param.constraint_map;
+        end
+        
+        % local neighbourhoods can be used in two ways:
+        %
+        % * the user provides a distance matrix (typically, sparse) with 0
+        % values for vertices outside the local neighbourhood.
+        %
+        % * the user does not provide a distance matrix, but specifies
+        %   the neighbourhood size with param.size
+        %
+        % if distance matrix is not provided by the user, it is computed
+        % extract parameters
+        if (~isfield(param, 'd') || isempty(param.d))
+            
+            % number of vertices in the mesh
+            N = size(x, 1);
+            
+            % compute full distance matrix from the mesh
+            d = zeros(N);
+            for I = 1:N
+                d(:, I) = perform_fast_marching_mesh(x, param.tri, I, options);
+            end
+            
+        end
+        
+
     case 'sphproj'
         
         % init output
