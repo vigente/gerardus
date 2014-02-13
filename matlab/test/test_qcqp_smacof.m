ee0d4bd47c38a21113e2a19f0d485c6afbf8eb8f
Ramón Casero <rcasero@gmail.com>
2014-02-13 00:43:53 +0000

    * Add matlab/ManifoldToolbox/tri_qcqp_smacof_nofold_2d.m: (0.0.1) 	* Add matlab/test/test_tri_qcqp_smacof_nofold_2d.m: (0.0.1)
    	- TRI_QCQP_SMACOF_NOFOLD_2D  Constraints for QCQP-SMACOF to ensure 
    	that a 2D triangular mesh has no fold-overs.
    
    	* Add matlab/test/test_qcqp_smacof.m: (0.0.2)
    
    	- Add test of a mesh without an untangled solution where we use 
    	tri_qcqp_smacof_nofold_2d constraints to find an untangled solution.

--- matlab/test/test_qcqp_smacof.m
+++ matlab/test/test_qcqp_smacof.m
@@ -2,7 +2,7 @@
 
 % Author: Ramon Casero <rcasero@gmail.com>
 % Copyright © 2014 University of Oxford
-% Version: 0.0.1
+% Version: 0.0.2
 % $Rev$
 % $Date$
 %
@@ -100,6 +100,149 @@ ub = [2 * ones(N, 1), 2 * ones(N, 1)];
 gplot(d, y, 'r')
 plot(y0(:, 1), y0(:, 2), '.k')
 plot(y(:, 1), y(:, 2), 'xr')
+
+stopCondition
+
+% plot stress evolution
+subplot(2, 1, 2)
+plot(t, sigma)
+xlabel('Time (sec)')
+ylabel('\sigma')
+
+%% Solve same toy example, now with constraints that prevent fold-overs 
+%% (i.e. triangles with negative area)
+
+% this will run slowly because we start from a random configuration
+
+% boundaries: maximum area of all the triangles in the mesh
+amax = max(trifacet_signed_area(tri, x));
+
+% boundaries: box within which coordinates must be located (we center the
+% box around zero, even though the toy example is not)
+ymin = [-1 -1] - 3;
+ymax = [5 5] - 3;
+
+% matrices and vectors for QCQP (assuming all vertices are free)
+[lb, ub, w, A, rl, ru, qc] ...
+    = tri_scip_nofold_cons_2d(tri, ymin, ymax, 1.5 * amax);
+
+% solve MDS problem with QCQP-SMACOF with constraints
+[y, stopCondition, sigma, t] ...
+    = qcqp_smacof(d, y0, lb, ub, w, A, rl, ru, qc, ...
+    smacof_opts, scip_opts);
+
+% rigid registration with true solution
+[~, y] = procrustes(x, y, 'Scaling', false);
+
+% plot points
+subplot(2, 1, 1)
+hold off
+gplot(d, x)
+hold on
+plot(x(:, 1), x(:, 2), 'o')
+
+% plot solution
+gplot(d, y, 'r')
+plot(y0(:, 1), y0(:, 2), '.k')
+plot(y(:, 1), y(:, 2), 'xr')
+
+stopCondition
+
+% plot stress evolution
+subplot(2, 1, 2)
+plot(t, sigma)
+xlabel('Time (sec)')
+ylabel('\sigma')
+
+%% Toy example where the optimal solution produces triangle overlap, and we
+%% use constraints to avoid the fold-over
+
+% toy example without fold-overs
+x = [
+    0 0
+    3 0
+    3 1
+    0 1
+    1 .6
+    2 .5
+    ];
+
+tri = [
+    2 6 1
+    2 3 6
+    6 3 5
+    3 4 5
+    5 4 1
+    6 5 1
+    ];
+
+d = dmatrix_mesh(tri, x);
+
+% compute areas to make sure that we have fold-overs
+a = trifacet_signed_area(tri, x);
+
+% plot toy example
+subplot(1, 1, 1)
+hold off
+gplot(d, x)
+axis equal
+
+% switch two vertices to create the fold-overs
+aux = x(5, :);
+x(5, :) = x(6, :);
+x(6, :) = aux;
+d = dmatrix_mesh(tri, x);
+
+% plot toy example
+subplot(2, 1, 1)
+hold off
+gplot(d, x)
+hold on
+plot(x(:, 1), x(:, 2), 'o')
+axis equal
+
+% compute areas to make sure that we have fold-overs
+a = trifacet_signed_area(tri, x);
+
+% boundaries: maximum area of all the triangles in the mesh
+amax = max(a);
+
+% boundaries: box within which coordinates must be located
+ymin = [-1 -1];
+ymax = [4 3];
+amin = .25;
+amax = 1.5 * amax;
+
+% matrices and vectors for QCQP (assuming all vertices are free)
+[lb, ub, w, A, rl, ru, qc] ...
+    = tri_qcqp_smacof_nofold_2d(tri, ymin, ymax, amin, amax);
+
+% SMACOF algorithm parameters
+smacof_opts.MaxIter = 100;
+smacof_opts.Epsilon = 1e-3;
+smacof_opts.Display = 'iter';
+smacof_opts.TolFun = 1e-6;
+
+% SCIP algorithm parameters
+scip_opts.display = 'off';
+scip_opts.warnings = 'off';
+scip_opts.maxiter = 1500;
+scip_opts.maxnodes = 10000;
+scip_opts.maxtime = 100;
+
+% solve MDS problem with QCQP-SMACOF with constraints
+% we use the mesh with fold-up as starting point
+[y, stopCondition, sigma, t] ...
+    = qcqp_smacof(d, x, lb, ub, w, A, rl, ru, qc, ...
+    smacof_opts, scip_opts);
+
+% plot solution
+subplot(2, 1, 2)
+hold off
+gplot(d, y)
+hold on
+plot(y(:, 1), y(:, 2), 'o')
+axis equal
 
 stopCondition
 
