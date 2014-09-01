296d1f180cc5fba326f202c12aa2708997888438
Ramón Casero <rcasero@users.noreply.github.com>
2014-09-01 11:12:06 +0000

    * matlab/CardiacToolbox/histology_blockface_preprocessing.m: (0.2.0)
    	- Now we input the elliptical (heart outline) and the polygonal
    	(wax blockface) masks.
    
    	- Correct scratches in block face.
    
    	- Instead of finding threshold between background and foreground
    	as the intersection of two gaussians, which doesn't work too well,
    	we simply find the mode of intensities. Because there's a lot more
    	background than tissue, the mode corresponds to the most typical
    	background value. We then make all intensities that are <= the
    	mode equal to zero. This is a very conservative threshold, that
    	removes some background, but preserves practically all tissue.
    
    	- We then extend the histograms of blockface and histology to
    	cover the whole intensity range.

--- matlab/CardiacToolbox/histology_blockface_preprocessing.m
+++ matlab/CardiacToolbox/histology_blockface_preprocessing.m
@@ -1,29 +1,35 @@
-function [imh, imbf] = histology_blockface_preprocessing(imh, imbf, maskbf)
-% HISTOLOGY_BLOCKFACE_PREPROCESSING  Prepare histology and blockface for
+function [imh, imbf] = histology_blockface_preprocessing(imh, imbf, polymask, ellipmask)
+% histology_blockface_preprocessing  Prepare histology and blockface for
 % registration.
 %
 % histology_blockface_preprocessing takes a histology and blockface images
 % that are going to be registered onto each other, and: 1) inverts them, to
-% have a dark background, 2) thresholds the background, to remove noise, 3)
-% extends the intensity range and 4) matches the histogram of the histology
-% image to the blockface.
+% have a dark background, 2) corrects the scratches of the blockface, 3)
+% thresholds the background, to remove noise, 4) extends the histograms to
+% cover the intensity range and 5) matches the histology histogram to the
+% blockface histogram.
 %
-% [IMH2, IMBF2] = histology_blockface_preprocessing(IMH, IMBF)
+% This function should be run after homogenising the illumination in the
+% blockface with blockface_equalise_illumination. It is also assumed that
+% the blockface has been rotated to make the scratches horizontal.
+%
+% [IMH2, IMBF2] = histology_blockface_preprocessing(IMH, IMBF, POLYMASK, ELLIPMASK)
 %
 %   IMH, IMBF are colour or grayscale images with the histology and
 %   blockface, respectively.
 %
+%   POLYMASK, ELLIPMASK are segmentation masks for the blockface image
+%   IMBF. POLYMASK is an outline of the wax blockface. ELLIPMASK is an
+%   ellipse that contains the heart. The masks can be created with
+%   blockface_create_masks.
+%
 %   IMH2, IMBF2 are the corresponding images after preprocessing.
 %
-% ... = histology_blockface_preprocessing(..., MASKBF)
-%
-%   MASKBF is a binary segmentation with the same size as IMBF. MASKF
-%   contains a binary mask. IMBF will be cropped to the smallest rectangle
-%   that contains that binary mask.
+% See also: blockface_create_masks, blockface_equalise_illumination.
 
 % Author: Ramon Casero <rcasero@gmail.com>
 % Copyright © 2014 University of Oxford
-% Version: 0.1.0
+% Version: 0.2.0
 % $Rev$
 % $Date$
 %
@@ -54,8 +60,15 @@ function [imh, imbf] = histology_blockface_preprocessing(imh, imbf, maskbf)
 DEBUG = 0;
 
 % check arguments
-narginchk(2, 3);
+narginchk(4, 4);
 nargoutchk(0, 2);
+
+if (any(size(imbf) ~= size(polymask)))
+    error('POLYMASK must be the same size as IMBF')
+end
+if (any(size(imbf) ~= size(ellipmask)))
+    error('ELLIPMASK must be the same size as IMBF')
+end
 
 %% preprocessing of histology
 
@@ -79,17 +92,19 @@ end
 
 %% Histology preprocessing
 
-% invert image 
+% invert image and make lowest intensity = 0
 imh = max(imh(:)) - imh;
 
-% find threshold between background and tissue
-thr = gmthr_seg(double(imh));
+% remove background by zeroing anything <= mode (the background forms the
+% largest peak). We are quite conservative here in terms of keeping all the
+% tissue we can, to avoid losing detail, even if that means that we are
+% going to have a bit of background noise
+imh(imh <= mode(double(imh(:)))) = 0;
 
-% remove background
-imh(imh <= thr) = 0;
-
-% normalize histogram
-imh = uint8(double(imh) / double(max(imh(:))) * 255);
+% extend histogram to cover whole dynamic range
+minh = double(min(imh(imh > 0)));
+maxh = double(max(imh(imh > 0)));
+imh = uint8(255 * (double(imh) - minh) / (maxh - minh));
 
 % plot preprocessed histology
 if (DEBUG)
@@ -99,53 +114,48 @@ end
 
 %% Blockface preprocessing
 
-% if a blockface mask is provided, then we crop the blockface image to the
-% smallest rectangle that contains the mask
-if (nargin > 2)
-    if (any(size(maskbf) ~= size(imbf)))
-        error('Blockface mask must be the same size as blockface image')
-    end
-    
-    % top/bottom limits of the mask
-    idx = find(sum(maskbf, 2) > 0);
-    top = idx(1);
-    bot = idx(end);
-    
-    % left/right limits of the mask
-    idx = find(sum(maskbf, 1) > 0);
-    lef = idx(1);
-    rig = idx(end);
-    
-    % crop blockface image
-    imbf = imbf(top:bot, lef:rig);
-    
-end
-
 % invert image
 imbf = max(imbf(:)) - imbf;
 
-% find threshold between background and tissue
-thr = gmthr_seg(double(imbf(:)));
+% correct scratches
 
-% threshold background
-imbf(imbf < thr) = 0;
+% duplicate of the image with intensity values only on the block without
+% the heart area, and the rest as 0
+imbf2 = imbf;
+imbf2(~xor(polymask, ellipmask)) = 0;
 
-% plot preprocessed blockface
-if (DEBUG)
-    subplot(2, 1, 2)
-    imagesc(imbf)
+% median intensity value of all intensity values on the block
+vtarget = double(median(imbf2(imbf2 > 0)));
+
+% iterate rows
+for I = 1:size(imbf, 1)
+   
+    % intensity values in this row that are not 0
+    v = double(imbf2(I, :));
+    idx = v > 0;
+    
+    % median intensity value in this row
+    vmed = median(v(idx));
+    
+    % correct intensity in this row to match the target intensity
+    imbf(I, idx) = double(imbf(I, idx)) / vmed * vtarget;
 end
 
-% fill the range of blockface intensities
-idx = imbf>0;
-imin = min(imbf(idx));
-imax = max(imbf(idx));
-imbf(:) = double(imbf(:) - imin(:)) / double(imax - imin) ...
-    * double(intmax(class(imbf)));
+% the largest peak in the histogram of the wax area gives as the threshold
+% background intensity
+imbf(imbf <= mode(double(imbf(polymask)))) = 0;
+
+% zero out the area outside the mask
+imbf(~polymask) = 0;
+
+% extend histogram to cover whole dynamic range
+minbf = double(min(imbf(imbf > 0)));
+maxbf = double(max(imbf(imbf > 0)));
+imbf = uint8(255 * (double(imbf) - minbf) / (maxbf - minbf));
 
 % match histology histogram to blockface's
 idx = imh > 0;
-imh(idx) = imhistmatch(imh(idx), imbf(imbf>0));
+imh(idx) = imhistmatch(imh(idx), imbf(ellipmask & (imbf > 0)));
 
 % plot slices and histograms
 if (DEBUG)
