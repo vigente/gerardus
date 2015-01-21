b5e9d75944a0e1de98b587eb70c873a361c1463b
rcasero <rcasero@e9491f86-7216-11de-921c-dbe4921c5040>
2015-01-21 17:26:36 +0000

    * matlab/FilesFormatToolbox/load_dcm.m (0.1.1)
    	- Fix help header. Part of the copyright notice missing. Help
    	header has to be separated from copyright notice. Make similar to
    	other Gerardus functions.

--- trunk/matlab/FileFormatToolbox/load_dcm.m
+++ trunk/matlab/FileFormatToolbox/load_dcm.m
@@ -1,22 +1,29 @@
 function [dcm, dcminfo] = load_dcm(files, pth)
-%% LOAD_DCM  Creates two 4-D arrays from DICOM series Data files.
+% LOAD_DCM  Load array of DICOM filenames as 3D+t image and metainformation
+% matrix.
 %
-% LOAD_DCM reads the data paths acquired via Christopher Kelly's LOAD_MULTI_DCM_DIR function. 
-% The data is arranged in a 2D array containing the dicominfo (metainfo) for each individual
-% slice across time, and a 4D array containing the slices (.dcm) with the
-% rows, columns, slice and time.
+% [DCM, DCMINFO] = LOAD_DCM(FILES, PTH)
 %
-% "S" represents the Slices, and "T" represents the Frames, which are
-% temporal data. 
-% 
-% The dcm output has for format: dcm(r,c,S,T).
-% The dcminfo output has for format: dcminfo(S,T)
-% Where "r" and "c" are the rows and columns respectively of the slice "S"
-% belonging to frame "T". 
+%   FILES, PTH are the output variables of function load_multi_dcm_dir().
 %
+%   FILES is an array of structs with information for each DICOM file in
+%   the set. FILES(I) corresponds to the I-th slice. Each slice contains
+%   files for each temporal frame.
+%
+%   PTH is a string with the path to the DICOMDIR file.
+%
+%   DCM is a 4-D array with the DICOM images. The format is 
+%   DCM(row, column, slice, time frame).
+%
+%   DCMINFO is a matrix of structs. DCMINFO(slice, time frame) contains the
+%   metainformation for the corresponding slice and time frame in the data
+%   set.
+%
+% See also: load_multi_dcm_dir.
+
 % Authors: Ramon Casero <rcasero@gmail.com> and Benjamin Villard
 % <b.016434@gmail.com>
-% Copyright © 2014-2015 University of Oxford
+% Copyright Â© 2014-2015 University of Oxford
 % Version: 0.1.0
 % $Rev$
 % $Date$
@@ -28,7 +35,24 @@ function [dcm, dcminfo] = load_dcm(files, pth)
 % This file is part of Gerardus.
 %
 % http://code.google.com/p/gerardus/
-
+%
+% This program is free software: you can redistribute it and/or modify
+% it under the terms of the GNU General Public License as published by
+% the Free Software Foundation, either version 3 of the License, or
+% (at your option) any later version.
+%
+% This program is distributed in the hope that it will be useful,
+% but WITHOUT ANY WARRANTY; without even the implied warranty of
+% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+% GNU General Public License for more details. The offer of this
+% program under the terms of the License is subject to the License
+% being interpreted in accordance with English Law and subject to any
+% action against the University of Oxford being under the jurisdiction
+% of the English Courts.
+%
+% You should have received a copy of the GNU General Public License
+% along with this program.  If not, see
+% <http://www.gnu.org/licenses/>.
 
 % check arguments
 narginchk(1, 2);
