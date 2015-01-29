d4325fa2c3cd0851b5e72a727481cb0df8d87e5c
Benjamin Villard <b.016434@gmail.com>
2015-01-29 19:27:44 +0000

    - Modified dcm2scimat.m Added temporal dimension to the axis properties.

--- matlab/FileFormatToolbox/dcm2scimat.m
+++ matlab/FileFormatToolbox/dcm2scimat.m
@@ -17,14 +17,17 @@ function scimat = dcm2scimat(dcm,dcminfo)
 %   
 %           data:   An array containing the dicom images, in a volume format. Data
 %                   can be 2D, 3D, or 4D (3D + time). 
-%           axis:   A 3-vector with the following fields:
+%           axis:   A 4-vector with the following fields:
 %                   
-%                   size: Image size.
+%                   size: Image size and temporal size (number of frames)
 %                    
-%                   spacing: Voxel size, i.e. spacing between voxel centres. 
+%                   spacing: Voxel size, i.e. spacing between voxel
+%                   centres. And temporal spacing (Nominal interval between
+%                   frames). 
 %                    
 %                   min: Coordinates of the "bottom-left" corner of the image, NOT
-%                       of the first voxel's centre. The first voxel's centre
+%                       of the first voxel's centre. The first voxel's
+%                       centre. And 
 % 
 %           rotmat: A (3, 3)- matrix that gives the image orientation as a rotation
 %                   of the x, y, z Cartesian axes.
@@ -40,7 +43,7 @@ function scimat = dcm2scimat(dcm,dcminfo)
 %
 % Authors:Benjamin Villard <b.016434@gmail.com>
 % Copyright © 2014-2015 University of Oxford
-% Version: 0.1.0
+% Version: 0.2.0
 % $Rev$
 % $Date$
 %
@@ -90,29 +93,46 @@ scimat.data = dcm;
 
 scimat.axis(1).size = size(dcm,1);% Resolution in mm
 scimat.axis(2).size = size(dcm,2);
-scimat.axis(3).size = size(dcm,3); 
+scimat.axis(3).size = size(dcm,3);
+if (ndims(scimat.data) > 3)
+    scimat.axis(4).size = size(dcm,4); 
+else
+    scimat.axis(4).size = 1;
+end
 
 % Check if Slices are 3D. NOTE: the information concerning the spacing of
 % the pixel/voxel was taken from the first file info. This is because we
 % assume a consistency in the imaging acquisition. The resolution of the
 % image should not change in space and time. 
-if isempty(dcminfo(1,1).SliceThickness)
-    vol = 1; % Slice Thickness is NOT empty --> Voxel (3D)
+if isempty(dcminfo(1,1).SliceLocation) % Slice Location is empty --> Pixel (2D)
+    scimat.axis(1).spacing = dcminfo(1,1).PixelSpacing(1);% Resolution in mm
+    scimat.axis(2).spacing = dcminfo(1,1).PixelSpacing(2);
+    scimat.axis(3).spacing = 1;
+    %Check if data is temporal
+    if (ndims(scimat.data) > 3)
+        scimat.axis(4).spacing = dcminfo(1,2).TriggerTime; 
+    else
+        scimat.axis(4).spacing = 1;
+    end
 else
-    vol = 2; % Slice Thickness is empty --> Pixel (2D)
-end
-
-switch vol
-    case 1
-        scimat.axis(1).spacing = dcminfo(1,1).PixelSpacing(1);% Resolution in mm
-        scimat.axis(2).spacing = dcminfo(1,1).PixelSpacing(2);
-        scimat.axis(3).spacing = 1; 
-        
-    case 2
-        scimat.axis(1).spacing = dcminfo(1,1).PixelSpacing(1);% Resolution in mm
-        scimat.axis(2).spacing = dcminfo(1,1).PixelSpacing(2);
-        scimat.axis(3).spacing = dcminfo(1,1).SliceThickness; 
-        
+    % Slice Thickness is NOT empty --> Voxel (3D)
+    scimat.axis(1).spacing = dcminfo(1,1).PixelSpacing(1);% Resolution in mm
+    scimat.axis(2).spacing = dcminfo(1,1).PixelSpacing(2);
+    %Check if multiple slices or whether data is just one slice but
+    %temporal.
+    for S = 1:size(dcminfo,1)
+        if size(dcminfo,1) == 1 % So that NaN errors don't come up due to the mean(diff())
+            scimat.axis(3).spacing = abs(mean([dcminfo(:,1).SliceLocation])); 
+        else
+            scimat.axis(3).spacing = abs(mean(diff([dcminfo(:,1).SliceLocation])));
+        end
+    end
+    %Check if data is temporal
+    if (ndims(scimat.data) > 3)
+        scimat.axis(4).spacing = dcminfo(1,2).TriggerTime; 
+    else
+        scimat.axis(4).spacing = 1;
+    end
 end
 
 % left edge of first voxel. This metric is different for each Slice, but consistent
@@ -120,11 +140,15 @@ end
 % the slices. Also we assume notation is the following: data(rows,columns,slice,time). 
 % As slice position is equal throughout time, T = 1;  
 
-for S = 1:size(scimat.data,3)
-    scimat.axis(1).min(S) = dcminfo(S,1).ImagePositionPatient(2); % min Y position of the left edge of the first voxel 
-    scimat.axis(2).min(S) = dcminfo(S,1).ImagePositionPatient(1); % min X position of the left edge of the first voxel 
-    scimat.axis(3).min(S) = dcminfo(S,1).ImagePositionPatient(3); % min Z position of the left edge of the first voxel 
-end
+    scimat.axis(1).min = dcminfo(1).ImagePositionPatient(2); % min Y position of the left edge of the first voxel 
+    scimat.axis(2).min = dcminfo(1).ImagePositionPatient(1); % min X position of the left edge of the first voxel 
+    scimat.axis(3).min = dcminfo(1).ImagePositionPatient(3); % min Z position of the left edge of the first voxel
+    if (ndims(scimat.data) > 3)
+        scimat.axis(4).min = dcminfo(1,1).TriggerTime;% min of Trigger time is 0.  
+    else
+        scimat.axis(4).min = 0;% min of Trigger time is 0.
+    end
+
 % Rotation Matrix. Same throughout space and time. 
     scimat.rotmat = [ dcminfo(1,1).ImageOrientationPatient(1:3),...
                                                     dcminfo(1,1).ImageOrientationPatient(4:6),...
