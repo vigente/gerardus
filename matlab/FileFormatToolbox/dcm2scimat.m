43a432695c9332caf0ceba3805653b9f79678f3f
Ramón Casero <rcasero@gmail.com>
2015-02-12 18:09:39 +0000

    * matlab/FileFormatToolbox/dcm2scimat.m: (0.3.0)
    	- Now the user can directly provide a DICOM filename instead of
    	having to load the image and metadata separately.

--- matlab/FileFormatToolbox/dcm2scimat.m
+++ matlab/FileFormatToolbox/dcm2scimat.m
@@ -1,5 +1,20 @@
-function scimat = dcm2scimat(dcm,dcminfo)
-% DCM2SCIMAT  Converts dicom datastructure to scimat format.
+function scimat = dcm2scimat(dcm, dcminfo)
+% DCM2SCIMAT  Load DICOM file or convert DICOM image data into SCIMAT
+% format.
+%
+% SCIMAT = DCM2SCIMAT(FILENAME)
+%
+%   FILENAME is a string with the path and name of a DICOM file.
+%
+%   SCIMAT is a struct used in Gerardus to store images and their metadata
+%   (see scimat.m for more details).
+%
+%   Note 1: DICOM uses mm and ms as spatial and temporal units, but we
+%   convert them to metres and seconds, respectively.
+%
+%   Note 2: DICOM direction cosines define a left-multiplying rotation
+%   matrix. We transpose it to make it right-multiplying, because in Matlab
+%   points are usually given as row vectors.
 %
 % SCIMAT = DCM2SCIMAT(DCM, DCMINFO)
 %
@@ -9,15 +24,13 @@ function scimat = dcm2scimat(dcm,dcminfo)
 %   DCMINFO is a matrix of structs. DCMINFO(slice, time frame) contains the
 %   metainformation for the corresponding slice and time frame in the data
 %   set.
-%
-%   SCIMAT is a struct used in Gerardus to store images and their metadata
-%   (see scimat.m for more details).
 % 
 % See also: scimat.m.
 
-% Authors:Benjamin Villard <b.016434@gmail.com>
+% Authors: Benjamin Villard <b.016434@gmail.com>,
+% Ramon Casero <rcasero@gmail.com>
 % Copyright © 2014-2015 University of Oxford
-% Version: 0.2.0
+% Version: 0.3.0
 % $Rev$
 % $Date$
 %
@@ -51,18 +64,19 @@ function scimat = dcm2scimat(dcm,dcminfo)
 narginchk(1, 2);
 nargoutchk(0, 1);
 
-% defaults
-if (nargin < 1 || isempty(dcm))
-    dcm = [];
-end
-if (nargin < 2 || isempty(dcminfo))
-    dcminfo = [];
+% if the user provides only one input argument, we assume that it is a
+% filename to load
+if (nargin == 1)
+    filename = dcm;
+    dcm = dicomread(filename);
+    dcminfo = dicominfo(filename);
 end
 
-% data volume (Dicom Image)
+%% image volume (Dicom Image)
 scimat.data = dcm;
 
-scimat.axis(1).size = size(dcm,1);% Resolution in mm
+%% image metadata
+scimat.axis(1).size = size(dcm,1);
 scimat.axis(2).size = size(dcm,2);
 scimat.axis(3).size = size(dcm,3);
 if (ndims(scimat.data) > 3)
@@ -76,31 +90,31 @@ end
 % assume a consistency in the imaging acquisition. The resolution of the
 % image should not change in space and time. 
 if isempty(dcminfo(1,1).SliceLocation) % Slice Location is empty --> Pixel (2D)
-    scimat.axis(1).spacing = dcminfo(1,1).PixelSpacing(1);% Resolution in mm
-    scimat.axis(2).spacing = dcminfo(1,1).PixelSpacing(2);
+    scimat.axis(1).spacing = dcminfo(1,1).PixelSpacing(1) * 1e-3; % units: m
+    scimat.axis(2).spacing = dcminfo(1,1).PixelSpacing(2) * 1e-3; % units: m
     scimat.axis(3).spacing = 1;
-    %Check if data is temporal
+    % check if data is temporal
     if (ndims(scimat.data) > 3)
-        scimat.axis(4).spacing = dcminfo(1,2).TriggerTime; 
+        scimat.axis(4).spacing = dcminfo(1,2).TriggerTime * 1e-3; % units: seconds
     else
         scimat.axis(4).spacing = 1;
     end
 else
     % Slice Thickness is NOT empty --> Voxel (3D)
-    scimat.axis(1).spacing = dcminfo(1,1).PixelSpacing(1);% Resolution in mm
-    scimat.axis(2).spacing = dcminfo(1,1).PixelSpacing(2);
+    scimat.axis(1).spacing = dcminfo(1,1).PixelSpacing(1) * 1e-3; % units: m
+    scimat.axis(2).spacing = dcminfo(1,1).PixelSpacing(2) * 1e-3; % units: m
     %Check if multiple slices or whether data is just one slice but
     %temporal.
     for S = 1:size(dcminfo,1)
         if size(dcminfo,1) == 1 % So that NaN errors don't come up due to the mean(diff())
-            scimat.axis(3).spacing = abs(mean([dcminfo(:,1).SliceLocation])); 
+            scimat.axis(3).spacing = abs(mean([dcminfo(:,1).SliceLocation])) * 1e-3; % units: m 
         else
-            scimat.axis(3).spacing = abs(mean(diff([dcminfo(:,1).SliceLocation])));
+            scimat.axis(3).spacing = abs(mean(diff([dcminfo(:,1).SliceLocation]))) * 1e-3; % units: m
         end
     end
     %Check if data is temporal
     if (ndims(scimat.data) > 3)
-        scimat.axis(4).spacing = dcminfo(1,2).TriggerTime; 
+        scimat.axis(4).spacing = dcminfo(1,2).TriggerTime * 1e-3; % units: seconds
     else
         scimat.axis(4).spacing = 1;
     end
@@ -111,20 +125,19 @@ end
 % the slices. Also we assume notation is the following: data(rows,columns,slice,time). 
 % As slice position is equal throughout time, T = 1;  
 
-    scimat.axis(1).min = dcminfo(1).ImagePositionPatient(2) - scimat.axis(1).spacing/2; % min Y position of the left edge of the first voxel 
-    scimat.axis(2).min = dcminfo(1).ImagePositionPatient(1) - scimat.axis(2).spacing/2; % min X position of the left edge of the first voxel 
-    scimat.axis(3).min = dcminfo(1).ImagePositionPatient(3) - scimat.axis(3).spacing/2; % min Z position of the left edge of the first voxel
-    if (ndims(scimat.data) > 3)
-        scimat.axis(4).min = dcminfo(1,1).TriggerTime;% min of Trigger time is 0.  
-    else
-        scimat.axis(4).min = 0;% min of Trigger time is 0.
-    end
+scimat.axis(1).min = dcminfo(1).ImagePositionPatient(2) * 1e-3 - scimat.axis(1).spacing/2; % min Y position of the left edge of the first voxel
+scimat.axis(2).min = dcminfo(1).ImagePositionPatient(1) * 1e-3 - scimat.axis(2).spacing/2; % min X position of the left edge of the first voxel
+scimat.axis(3).min = dcminfo(1).ImagePositionPatient(3) * 1e-3 - scimat.axis(3).spacing/2; % min Z position of the left edge of the first voxel
+if (ndims(scimat.data) > 3)
+    scimat.axis(4).min = dcminfo(1,1).TriggerTime  * 1e-3;% min of Trigger time is 0.
+else
+    scimat.axis(4).min = 0;% min of Trigger time is 0.
+end
 
 % Rotation Matrix. Same throughout space and time. 
-    scimat.rotmat = [ dcminfo(1,1).ImageOrientationPatient(1:3),...
-                                                    dcminfo(1,1).ImageOrientationPatient(4:6),...
-                                                    cross(dcminfo(1,1).ImageOrientationPatient(1:3), dcminfo(1,1).ImageOrientationPatient(4:6)) ];
+scimat.rotmat = [ dcminfo(1,1).ImageOrientationPatient(1:3),...
+    dcminfo(1,1).ImageOrientationPatient(4:6),...
+    cross(dcminfo(1,1).ImageOrientationPatient(1:3), ...
+    dcminfo(1,1).ImageOrientationPatient(4:6)) ]';
 
 scimat.axis = scimat.axis'; 
-end 
-
