9b134fe287375eecae8ae35e9d25666a5caf41f1
darryl.mcclymont@gmail.com <Darryl-McClymont@users.noreply.github.com>
2015-03-12 18:29:15 +0000

    2014-03-12  Darryl McClymont  <darryl.mcclymont@gmail.com>
    	* add matlab/DiffusionMRIToolbox/weighted_linear_fit.m (0.1.0)
    
    	- WEIGHTED_LINEAR_FIT fits the function Y = MX (matrices) such that the
    	residuals follow a gaussian distribution after having a certain function
    	applied to them. It is good when you have a nonlinear function that you 
    	can easily linearise, but you want a result closer to the nonlinear fit,
    	and you don’t know what weighting vector to use.
    
    	* matlab/DiffusionMRIToolbox/fit_stretched_exponential.m (0.1.4)
    
    	- Now uses weighted linear fit (see above). Also now has extra output for
    	S0. Jacobian is now used in non-linear fitting, speeding this up by around 20%.

--- matlab/DiffusionMRIToolbox/fit_stretched_exponential.m
+++ matlab/DiffusionMRIToolbox/fit_stretched_exponential.m
@@ -1,4 +1,4 @@
-function [AA, FD, gamma_all, A_all, Npoints] = fit_stretched_exponential(I0, Ihigh, bval0, bvalhigh, mask, noise_thresh)
+function [AA, FD, S0_all, gamma_all, A_all, Npoints] = fit_stretched_exponential(I0, Ihigh, bval0, bvalhigh, mask, noise_thresh)
 % FIT_STRETCHED_EXPONENTIAL    Fit the stretched exponential model of 
 % diffusion to an image
 %
@@ -40,17 +40,18 @@ function [AA, FD, gamma_all, A_all, Npoints] = fit_stretched_exponential(I0, Ihi
 %   Outputs:
 %       AA is Anomalous Anisotropy [r c s]
 %       FD is Fractal Dimension [r c s]
+%       S0_ALL are the coefficients of S0 [r c s dir]
 %       GAMMA_ALL are the coefficients of gamma [r c s dir]
 %       A_ALL are the coefficients of A [r c s dir]
 %       NPOINTS is the number of fit shells at each voxel (see Hall 2008 
 %           Figure 3e)       
 %
-%   See also fit_DT
+%   See also fit_DT, weighted_linear_fit
 
 
 % Author: Darryl McClymont <darryl.mcclymont@gmail.com>
 % Copyright © 2014 University of Oxford
-% Version: 0.1.3
+% Version: 0.1.4
 % $Rev$
 % $Date$
 % 
@@ -79,7 +80,7 @@ function [AA, FD, gamma_all, A_all, Npoints] = fit_stretched_exponential(I0, Ihi
 
 % check arguments
 narginchk(4, 6);
-nargoutchk(0, 5);
+nargoutchk(0, 6);
 
 if (nargin < 5) || isempty(mask)
     mask = 0;
@@ -99,91 +100,142 @@ I0_mean = mean(I0, 4);
 
 gamma_all = zeros(sz(1:4));
 A_all = zeros(sz(1:4));
-
+S0_all = zeros(sz(1:4));
 %mask = (I0_mean > 2E4) & (I0_mean ./ Ihigh(:,:,:,1,1) < 3);
 
 
 % as in Hall 2008, we only want to fit the linear regression where we
 % are above the noise floor
 Imin = min(Ihigh, [], 4);
-Npoints = sum(Imin > noise_thresh, 5);
+Npoints = sum(Imin > noise_thresh, 5)+1; % (includes b = 0)
+
+nvoxels = prod(sz(1:3));
 
 for dir = 1:sz(4) % for each direction
-
-    b0 = bval0(:,:,1);
-    b1 = bvalhigh(:,:,dir,1);
-    b2 = bvalhigh(:,:,dir,2);
-    b3 = bvalhigh(:,:,dir,3);
-
-    % magnitude of b
-    b = [sqrt(sum(b1(:).^2)), sqrt(sum(b2(:).^2)), sqrt(sum(b3(:).^2))]';
-
-    % normalise by I0
-    I_dir = permute(Ihigh(:,:,:,dir,:), [1 2 3 5 4]) ./ repmat(I0_mean, [1 1 1 size(Ihigh,5)]);
+    disp(['Dir = ' num2str(dir)])
     
-    Ivector = reshape(I_dir, [prod(sz(1:3)), sz(5)]);
+    bval_all = cat(3, bval0, squeeze(bvalhigh(:,:,dir,:)));
     
-    coeff = zeros(prod(sz(1:3)), 2);
+    b = zeros(1, size(bval_all,3));
+    for nshell = 1:size(bval_all,3)
+        b(nshell) = bval_all(1,1,nshell) + bval_all(2,2,nshell) + bval_all(3,3,nshell);
+    end
+
+    %disp(b)
     
-    for n = 2:max(Npoints(:)) % n = number of points for linear fitting
+    I_all = cat(4, I0_mean, permute(Ihigh(:,:,:,dir,:), [1 2 3 5 4]));
+    
+    Ivector = reshape(I_all, [prod(sz(1:3)), size(I_all,4)]);
+    
+    S0 = Ivector(:,1);
+    A = zeros(nvoxels,1);
+    gamma = zeros(nvoxels,1);
+    
+    
+    for n = 3:max(Npoints(:)) % n = number of points for linear fitting
+        %disp(['Num shells = ' num2str(n)])
+        
         idx = Npoints(:) == n;
+        S0_idx = S0(idx);
+        
+        % eqn 1 - use S0 and solve for gamma, A
+        
+        
+        I_idx = Ivector(idx,1:n);
         
         % take the log for linear fitting
-        loglogsig = log(abs(log(Ivector(idx, 1:n))));
+        loglogsig = log(abs(log(eps + bsxfun(@rdivide, I_idx, S0_idx))));
         logb = log(b(1:n));
 
         b_array = [logb(:), ones(size(logb(:)))];
 
-        % alternatively, fit the linear model using a weighted fit
-    %     w = zeros(3);
-    %     w(1,1) = 1;
-    %     w(2,2) = 0.5;
-    %     w(3,3) = 0.1;
-    %     coeff(idx,:) = (pinv(b_array' * (w.^2) * b_array) * b_array' * (w.^2) * (loglogsig'))';
-        coeff(idx, :) = (b_array \ loglogsig')';
+
+        %coeff = (b_array(2:end,:) \ loglogsig(:,2:end)')';
+        func = @(x) exp(-abs(exp(x)));
+        coeff = weighted_linear_fit(loglogsig(:,2:end), b_array(2:end,:)', func,0);
+
+        gamma_idx = coeff(:,1);
+        A_idx = exp(coeff(:,2));
+
+%             sig_recon = S0_idx(1) .* exp(-A_idx(1) .* b(1:n).^gamma_idx(1));
+%             err = I_idx(1,:) - sig_recon;
+% 
+%             %rmse = S0_idx .* exp(A_idx .* b(1:n).^gamma_idx) - Ivector
+%             disp(['gamma = ' num2str(gamma_idx(1)), ...
+%                 ', A = ' num2str(A_idx(1)) ', S0 = ' num2str(S0_idx(1)), ', rmse = ' num2str(sqrt(mean(err.^2)))])
+% 
+%             % eqn 2 - use gamma and solve for S0, A
+%             gamma_idx = constrain(gamma_idx, 0, 1.5);
+%             logsignal = log(Ivector(idx,1:n));
+% 
+%             % the equation can be linearised in another way, as 
+%             % log(S) = log(S0) - A*b^gamma
+%
+%             for vox = 1:size(logsignal,1)
+%                 b_pow = b(1:n).^gamma_idx(vox);
+%                 b_array = [b_pow(:), ones(size(b_pow(:)))];
+% 
+%                 % only using n-1 here because I had issues from the log
+%                 % operation
+%                 %coeff = b_array(1:end-1,:) \ logsignal(vox,1:end-1)';
+%                 coeff = weighted_linear_fit(logsignal(vox,1:end), b_array(1:end,:)', @(x)exp(x), 0); 
+%                 
+%                 A_idx(vox) = -coeff(1);
+%                 S0_idx(vox) = exp(coeff(2));
+% 
+%             end
+%             
+%             sig_recon = S0_idx(1) .* exp(-A_idx(1) .* b(1:n).^gamma_idx(1));
+%             err = I_idx(1,:) - sig_recon;
+% 
+%             %rmse = S0_idx .* exp(A_idx .* b(1:n).^gamma_idx) - Ivector
+%             disp(['gamma = ' num2str(gamma_idx(1)), ...
+%                 ', A = ' num2str(A_idx(1)) ', S0 = ' num2str(S0_idx(1)), ', rmse = ' num2str(sqrt(mean(err.^2)))])
+
+        
+        gamma(idx) = gamma_idx;
+        A(idx) = A_idx;
+        S0(idx) = S0_idx;
+        
     end
     
     if max(mask(:)) == 1 % if the mask is not empty
     
-        % use x as a best guess for the non-linear fitting
-        I_dir = squeeze(Ihigh(:,:,:,dir,:)); % not normalised this time
-        b = [sqrt(sum(b0(:).^2)), sqrt(sum(b1(:).^2)), sqrt(sum(b2(:).^2)), sqrt(sum(b3(:).^2))];
-
-        % do the nonlinear fitting only on the voxels in the mask
-        Ivector = reshape(I_dir, [prod(sz(1:3)), sz(5)]);
-        Ivector = double([I0_mean(mask), Ivector(mask(:), :)]);
-
-        options = optimset('display','off');
+        
+        options = optimoptions('lsqcurvefit','Jacobian','on', 'DerivativeCheck', 'off', 'display', 'off');
         lb = zeros(1,3);
         ub = [1, 10E-3, inf]; % See Bennet et al. for bounds
 
-        new_coeffs = zeros(size(Ivector,1), 3);
-        old_coeffs = coeff(mask(:),:);
-
-        s = matlabpool('size');
-        if (s == 0) && (size(new_coeffs,1) > 100) % if there is a decent number of voxels
-            disp('Open matlab pool to use parallel processing')
+        x_guess = abs(double([gamma(mask(:)), A(mask(:)), Ivector(mask(:),1)]));
+        new_coeffs = zeros(size(x_guess));
+        
+        I_nlfit = double(Ivector(mask(:), :));
+        
+        
+        if (matlabpool('size') == 0) && (size(new_coeffs,1) > 100) % if there is a decent number of voxels
+            disp('Open matlab pool if you want to use parallel processing')
         end
         
         parfor i = 1:size(new_coeffs,1)
+
+            % alternative (slower) method:
+            %modelfun = @(x) x(3) .* exp(- x(2) * (b.^x(1))) - I_nlfit(i,:);
+            %new_coeffs(i,:) = lsqnonlin(modelfun, x_guess(i,:), lb, ub, options);
             
-            x_guess = double([old_coeffs(i,1), exp(old_coeffs(i,2)), Ivector(i,1)]);
-
-            modelfun = @(x) x(3) .* exp(- x(2) * (b.^x(1))) - Ivector(i,:);
-
-            new_coeffs(i,:) = lsqnonlin(modelfun, abs(x_guess), lb, ub, options);
+            new_coeffs(i,:) = lsqcurvefit(@stretched_exp_model, x_guess(i,:), b, I_nlfit(i,:), lb, ub, options);
+            
         end
 
-        coeff(mask(:), :) = new_coeffs(:,1:2);
+        gamma(mask(:)) = new_coeffs(:,1);
+        A(mask(:)) = new_coeffs(:,2);
+        S0(mask(:)) = new_coeffs(:,3);
+
     end
     
-    
-    gamma = reshape(coeff(:,1), sz(1:3));
-    
-    A = reshape(exp(coeff(:,2)), sz(1:3));
 
-    gamma_all(:,:,:,dir) = gamma;
-    A_all(:,:,:,dir) = A;
+    gamma_all(:,:,:,dir) = reshape(gamma, sz(1:3));
+    A_all(:,:,:,dir) =  reshape(A, sz(1:3));
+    S0_all(:,:,:,dir) = reshape(S0, sz(1:3));
 end
 
 % Anomalous anisotropy
@@ -195,3 +247,16 @@ AA = sqrt(N / (N - 1) * sum((gamma_all - repmat(mean_gamma, [1 1 1 N])).^2, 4)..
 % Fractal dimension
 FD = 2 / mean_gamma;
 
+
+
+% the function to fit inside the parfor loop
+function [F, J] = stretched_exp_model(x, b)
+
+F = x(3) .* exp(- x(2) * (b.^x(1)));
+
+if nargout > 1 % Jacobian - makes fitting about 20% faster
+    J = zeros(length(b), 3);
+    J(:,1) = -x(2) * x(3) * b.^(x(1)) .* log(b) .* exp(- x(2) * (b.^x(1)));
+    J(:,2) = - b.^(x(1)) * x(3) .* exp(- x(2) * (b.^x(1)));
+    J(:,3) = exp(- x(2) * (b.^x(1)));
+end
