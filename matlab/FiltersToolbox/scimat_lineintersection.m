f7131ceaf6575351e4813985b70e68b3593dfb58
Benjamin Villard <b.016434@gmail.com>
2015-05-06 17:33:54 +0100

    Added the Header file (Still not fully complete)

--- matlab/FiltersToolbox/scimat_lineintersection.m
+++ matlab/FiltersToolbox/scimat_lineintersection.m
@@ -1,10 +1,45 @@
 function [intensities1, intensities2,intersectCoords] =  scimat_lineintersection(scimat1, scimat2, coords1, coords2, res)
 
 
+% SCIMAT_LINEINTERSECTION Returns the line intensities from two
+% intersecting scimat files at a specified resolution. 
+
+
 % This function operated on a Slice - by - Slice method: 
 % Scimat1.data and Scimat2.data are 2D datasets 
 % However the data is in 3D space.
 % The user has the choice to specify the Slice and Frame wanted. 
+
+% Authors: Benjamin Villard <b.016434@gmail.com>, Christopher Kelly  <christopher.kelly28@googlemail.com>
+% Copyright © 2011-2015 University of Oxford
+% Version: 0.1.0
+% $Rev$
+% $Date$
+% 
+% University of Oxford means the Chancellor, Masters and Scholars of
+% the University of Oxford, having an administrative office at
+% Wellington Square, Oxford OX1 2JD, UK. 
+%
+% This file is part of Gerardus.
+%
+% This program is free software: you can redistribute it and/or modify
+% it under the terms of the GNU General Public License as published by
+% the Free Software Foundation, either version 3 of the License, or
+% (at your option) any later version.
+%
+% This program is distributed in the hope that it will be useful,
+% but WITHOUT ANY WARRANTY; without even the implied warranty of
+% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+% GNU General Public License for more details. The offer of this
+% program under the terms of the License is subject to the License
+% being interpreted in accordance with English Law and subject to any
+% action against the University of Oxford being under the jurisdiction
+% of the English Courts.
+%
+% You should have received a copy of the GNU General Public License
+% along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
 
 %% Default Checks:
 
