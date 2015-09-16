6b2a839e1b97723ab3d6bffa2bccda747638c252
Nicolas Basty <nicolas.basty@eng.ox.ac.uk>
2015-09-16 15:11:19 +0100

    The functions now support blocks that aren't just cubes, and a sliding window option was also added.

--- matlab/FiltersToolbox/col2im3.m
+++ matlab/FiltersToolbox/col2im3.m
@@ -1,18 +1,18 @@
-function [threeD] = col2im3(reshaped3D2D,blocksX,blocksY,blocksZ)
+function [threeD] = col2im3_edit2(col,mat,block,kind)
 %COL2IM3 rearranges matrix columns into a 3D image
 
-% 22 July 2015
+% 16 September 2015
 %
 % col2im3 the same as col2im, only for 3D arrays
-% It reshapes each of the columns of the 2D array into cubes, then to stacks them back together layer by layer
-% The inputs are a 2D array and numbers of blocks in each direction
-% outputs from im2col3
-% The output is a 3D array. The dimensions of which are
-% blocksize*blocksX by blocksize*blocksY by blocksize*blocksZ
+% It reshapes each of the columns of the 2D array into cubes, 
+% and stacks them back together
+% The inputs are a 2D array, the size of the output and the image blocks,
+% and the kind of window (sliding/distinct).
+% output from im2col3 is a 3D array.
 %
 % Author: Nicolas Basty <nicolas.basty@eng.ox.ac.uk>
 % Copyright ??? 2015 University of Oxford
-% Version: 0.1.0
+% Version: 0.2
 %
 % University of Oxford means the Chancellor, Masters and Scholars of
 % the University of Oxford, having an administrative office at
@@ -38,42 +38,43 @@ function [threeD] = col2im3(reshaped3D2D,blocksX,blocksY,blocksZ)
 % along with this program.  If not, see <http://www.gnu.org/licenses/>.
 % _________________________________________________________________
 
-blocksize = nthroot(size(reshaped3D2D,1),3); %cubic root gives size of a side of the cube
-
-% blocksZ, number of slabs (blocks in Z) from output from previous function
-% blocksX, number of blocks in x
-% blocksZ, number of blocks in y
-
-if  rem(blocksize,1) ~= 0 %checks if blocksize is a cubic number
-    fprintf('Input array is supposed to have a cubic number as first dimension! \n')
-    return
+if ~ischar(kind),
+    error(message('images:col2im:wrongBlockType'));
 end
 
-if blocksX*blocksY*blocksZ ~= size(reshaped3D2D,2)
-    fprintf('The number of blocks does not match the input array. \n')
-    return
-    %checks if blocksize makes sense
-end
+% size of output 3D matrix
+blocksize_x = block(1);
+blocksize_y = block(2);
+blocksize_z = block(3);
 
-blocknum = 1; %just initialising the block number
+Number_of_blocks_X = mat(1)/block(1); %how many blocks there are along each dimension
+Number_of_blocks_Y = mat(2)/block(2);
+Number_of_blocks_Z = mat(3)/block(3);
 
-threeD = zeros(blocksize*blocksX,blocksize*blocksY,blocksize*blocksZ);
+blocknum = 1; % initialising the block number
+
+threeD = zeros(blocksize_x*Number_of_blocks_X,blocksize_y*Number_of_blocks_Y,blocksize_z*Number_of_blocks_Z);
 %initialising the final array
 
-for slabn = 1:blocksZ %Z
-    %loop goes in the Z direction slab by slab
-    for xnum = 1:blocksX
-        for ynum = 1:blocksY
+if strcmp(kind, 'distinct') % Distinct
+    
+for slabn = 1:Number_of_blocks_Z
+    for ynum = 1:Number_of_blocks_X
+        for xnum = 1:Number_of_blocks_Y
             
-            temp = reshaped3D2D(:,blocknum);%temporarily stores the n-th block in Ax1 form
-            temp2 = reshape(temp,blocksize,blocksize,blocksize);%this reshapes the Ax1 into axaxa
-            
-            threeD(xnum+(blocksize*(xnum-1)-xnum+1):(xnum*blocksize),ynum+(blocksize*(ynum-1)-ynum+1):(ynum*blocksize),slabn+(blocksize*(slabn-1)-slabn+1):(slabn*blocksize)) = temp2;
+            temp = col(:,blocknum);%temporarily stores the n-th block in Ax1 form
+            temp2 = reshape(temp,blocksize_x,blocksize_y,blocksize_z);%this reshapes the Ax1 into axaxa
+            threeD(xnum+(blocksize_x*(xnum-1)-xnum+1):(xnum*blocksize_x),ynum+(blocksize_y*(ynum-1)-ynum+1):(ynum*blocksize_y),slabn+(blocksize_z*(slabn-1)-slabn+1):(slabn*blocksize_z)) = temp2;
             %stores the cube at the location within the big 3D array
-            
             blocknum = blocknum +1; %move on to next block
+        
         end
     end
 end
 
+elseif strcmp(kind, 'sliding') % sliding
+    threeD = reshape(col(floor(end/2),:),mat(1)-block(1)+1,mat(2)-block(2)+1,(size(col,2)/(mat(1)-block(1)+1))/(mat(2)-block(2)+1));
+    %might need to find another way of finding the representative intensity
+else
+    error(message('images:col2im:unknownBlockType', deblank(kind)))
 end
--- matlab/FiltersToolbox/im2col3.m
+++ matlab/FiltersToolbox/im2col3.m
@@ -1,17 +1,18 @@
-function [reshaped3D2D,blocknumx,blocknumy,blocknumz] = im2col3(image3D,blocksize)
-%IM2COL3 rearranges 3D image blocks into columns
+function [reshaped3D2D] = im2col3_edit2(varargin)
+%IM2COL3 rearranges 3D image blocks into columns. 
 
-% 21 July 2015
+% 16 September 2015
 %
-% im2col3 does the same as im2col, only for 3D arrays
-% It decomposes the 3D array into cubes, then to 2D (layer by layer)
-% The inputs are a 3D dataset and a number used for the wished blocksize
-% Blocksize has to be a common divisor of the three dimensions
-% The output is a 2D array of dimensions blocksize^3 by blocknumber
+% im2col3 does the same as im2col, only for 3D arrays.
+% The inputs are a 3D dataset and a vector for the blocksize, [x y z], 
+% and the kind of block ('sliding' or 'distinct')
+% x,y&z have to be divisors of the respective dimension
+% The output is a 2D array of dimensions xyz by the number of blocks 
+% Each column of xyz elements represents a 3D element
 %
 % Author: Nicolas Basty <nicolas.basty@eng.ox.ac.uk>
 % Copyright 2015 University of Oxford
-% Version: 0.1.0
+% Version: 0.2
 %
 % University of Oxford means the Chancellor, Masters and Scholars of
 % the University of Oxford, having an administrative office at
@@ -37,59 +38,127 @@ function [reshaped3D2D,blocknumx,blocknumy,blocknumz] = im2col3(image3D,blocksiz
 % along with this program.  If not, see <http://www.gnu.org/licenses/>.
 % _________________________________________________________________
 
-tic
+[image3D, block, kind] = parse_inputs(varargin{:});
+
+blocksize_x = block(1);
+blocksize_y = block(2);
+blocksize_z = block(3);
+
+blocknumx = length(1:blocksize_x:size(image3D,1)); %to find the number of blocks in each dimension
+blocknumy = length(1:blocksize_y:size(image3D,2));
+blocknumz = length(1:blocksize_z:size(image3D,3));
+blocknum = blocknumx*blocknumy*blocknumz; %total number of cubes in the 3D array
+
 
 if  numel(size(image3D)) ~= 3 %checks if input is 3D array
-    fprintf('Input array is expected to be 3D, not %dD! \n',numel(image3D))
+    fprintf('Input array is expected to be 3D, not %dD! \n',numel(size(image3D)))
     return
 end
 
-if mod(size(image3D,1),blocksize)+ mod(size(image3D,2),blocksize)+mod(size(image3D,3),blocksize)~= 0
-    fprintf('Blocksize is expected to be a common divisor of length of the sides of the array! \n')
-    return
-    %checks if blocksize is common divisor of the lengths of sides of the array
-end
-
-if  rem(blocksize,1) ~= 0 %checks if integer
+if  rem(blocksize_x,1) ~= 0 || rem(blocksize_y,1) ~= 0 || rem(blocksize_z,1) ~= 0 %checks if integer
     fprintf('Blocksize is expected to be an integer \n')
     return
 end
 
-blocknumx = length(1:blocksize:size(image3D,1)); %to find the number of blocks in each dimension
-blocknumy = length(1:blocksize:size(image3D,2));
-blocknumz = length(1:blocksize:size(image3D,3));
-
-blocknum = blocknumx*blocknumy*blocknumz; %total number of cubes in the 3D array
-
-fprintf('%d %dx%dx%d blocks inside the volume \n',blocknum, blocksize,blocksize,blocksize)
-
-bl=1; %this is the n-th cube number, used to index the final array
-reshaped3D2D = zeros(blocksize^3,blocknum); %initialising final array to store all the blocks in column form
-
-for slicenum=1:blocksize:(size(image3D,3))
+if strcmp(kind,'distinct')
     
-    slab = image3D(:,:,slicenum:slicenum+blocksize-1);
-    % cuts out a slab of dimensions of a 2D slice and height of blocksize
+    reshaped3D2D = zeros(blocksize_x*blocksize_y*blocksize_z,blocknum); %initialising final array to store all the blocks in column form
     
-    for vex=1:blocksize:(size(image3D,1)) %"ve" for vertex, x, y & z directions
-        for vey=1:blocksize:(size(image3D,2))
-            for vez=1:blocksize
-                reshaped3D2D(blocksize^2*(vez-1)+1:blocksize^2*vez,bl)=...
-                    reshape(slab(vex:(vex+blocksize-1),vey:(vey+blocksize-1),vez),[blocksize^2 1]);
-                %reshape each slice of the block and store within the final array
+    tic
+        
+    bl=1; %this is the n-th cube number, used to index the final array
+    
+    for slicenum=1:blocksize_z:(size(image3D,3))
+        
+        slab = image3D(:,:,slicenum:slicenum+blocksize_z-1);
+        % cuts out a slab of dimensions of a 2D slice and height of blocksize
+        
+        for edgy=1:blocksize_x:(size(image3D,1)) %"edg" for edge in x, y & z directions
+            for edgx=1:blocksize_y:(size(image3D,2))
+                for edgz=1:blocksize_z
+                    
+                    reshaped3D2D(blocksize_x*blocksize_y*(edgz-1)+1:blocksize_x*blocksize_y*edgz,bl)=...
+                        reshape(slab(edgx:(edgx+blocksize_x-1),edgy:(edgy+blocksize_y-1),edgz),[blocksize_x*blocksize_y 1]);
+                    %reshape each slice of the block and store within the final array
+                end
+                bl=bl+1; %proceed to next block after the Z dimension loop is done
             end
-            bl=bl+1; %proceed to next block after the Z dimension loop is done
+        end
+    end
+        
+end
+
+if strcmp(kind,'sliding')   
+    
+    bl=1; %this is the n-th cube number, used to index the final array
+
+    for slicenum=1:((size(image3D,3))-block(3)+1)
+        %loop ends when the last cube reaches the end
+        
+        slab = image3D(:,:,slicenum:slicenum+blocksize_z-1);
+        % cuts out a slab of dimensions of a 2D slice and height of blocksize        
+        
+        for edgy=1:1:(size(image3D,1)- blocksize_x +1) 
+            %"edg" for edge of the cube in x, y & z directions
+            %loop ends when the last cube reaches the end
+
+            for edgx=1:1:(size(image3D,2)- blocksize_y +1)
+                for edgz=1:blocksize_z                    
+        
+                    reshaped3D2D(blocksize_x*blocksize_y*(edgz-1)+1:blocksize_x*blocksize_y*edgz,bl)=...
+                        reshape(slab(edgx:(edgx+blocksize_x-1),edgy:(edgy+blocksize_y-1),edgz),[blocksize_x*blocksize_y 1]);
+                    %reshape each slice of the block and store within the final array
+                
+                end
+                
+                bl=bl+1; %proceed to next block after the Z dimension loop is done
+            
+            end
         end
     end
 end
 
-A = size(image3D,1); %for printout
-B = size(image3D,2);
-C = size(image3D,3);
-D = blocksize^3;
-E = blocknum;
-T = toc;
+%%% Function parse_inputs (taken from im2col)
+function [a, block, kind, padval] = parse_inputs(varargin)
 
-fprintf('%dx%dx%d 3D-array is now %dx%d \nTime elapsed is %2.2f seconds\n',A,B,C,D,E,T)
+narginchk(2,4);
 
+switch nargin
+    case 2
+        if (strcmp(varargin{2},'indexed'))
+            error(message('images:im2col:tooFewInputs'))
+        else
+            % IM2COL(A, [M N])
+            a = varargin{1};
+            block = varargin{2};
+            kind = 'sliding';
+            padval = 0;
+        end
+        
+    case 3
+        if (strcmp(varargin{2},'indexed'))
+            % IM2COL(A, 'indexed', [M N])
+            a = varargin{1};
+            block = varargin{3};
+            kind = 'sliding';
+            padval = 1;
+        else
+            % IM2COL(A, [M N], 'kind')
+            a = varargin{1};
+            block = varargin{2};
+            kind = validatestring(varargin{3},{'sliding','distinct'},mfilename,'kind',3);
+            padval = 0;
+        end
+        
+    case 4
+        % IM2COL(A, 'indexed', [M N], 'kind')
+        a = varargin{1};
+        block = varargin{3};
+        kind = validatestring(varargin{4},{'sliding','distinct'},mfilename,'kind',4);
+        padval = 1;
+        
+end
+
+if (isa(a,'uint8') || isa(a, 'uint16'))
+    padval = 0;
 end
