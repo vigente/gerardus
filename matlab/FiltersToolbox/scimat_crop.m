0d9c47d840fd854198e1132db49af0b1827b26b7
Ramón Casero <rcasero@gmail.com>
2016-12-06 17:19:06 +0000

    (0.5.0): Add new syntax so that a 3D image volume can be provided as a list of files, each with a 2D image.

--- matlab/FiltersToolbox/scimat_crop.m
+++ matlab/FiltersToolbox/scimat_crop.m
@@ -1,5 +1,13 @@
-function scimat = scimat_crop(scimat, from, to)
+function scimat = scimat_crop(scimat, from, to, ds)
 % SCIMAT_CROP  Crop a SCIMAT image or segmentation volume of any dimension.
+%
+% This function has two modes: Either crop a 1D to 5D scimat volume, or
+% crop a 3D volume given slice-by-slice by a list of image files. The
+% latter is convenient for creating virtual slices from a stack of high
+% resolution histology images.
+%
+% -------------------------------------------------------------------------
+% Syntax to crop a 1D to 5D scimat volume:
 %
 % SCIMAT2 = scimat_crop(SCIMAT, FROM, TO)
 %
@@ -7,9 +15,10 @@ function scimat = scimat_crop(scimat, from, to)
 %   "help scimat" for details). The image can have any number of
 %   dimensions.
 %
-%   FROM, TO are vectors with the index coordinates that define the
-%   cropping box. The number of indices depend on the dimensionality of the
-%   image. For example, for a 3D volume, FROM=[2 3 7], TO=[15 20 22].
+%   FROM, TO are (row, col, slice) vectors with the index coordinates that
+%   define the cropping box. The number of indices depend on the
+%   dimensionality of the image. For example, for a 3D volume, FROM=[2 3
+%   7], TO=[15 20 22].
 %
 %   If FROM or TO don't have all the dimensions, those dimensions are not
 %   cropped. E.g. in an image with size [256 512 128 10],
@@ -34,10 +43,22 @@ function scimat = scimat_crop(scimat, from, to)
 %
 %   % crop struct
 %   scimat2 = scimat_crop(scimat, [50 75], [100 89]);
+%
+% -------------------------------------------------------------------------
+% Syntax to crop a 3D volume given slice-by-slice by a list of image files:
+%
+% SCIMAT = scimat_crop(FILE, FROM, TO)
+%
+%   FILE is a cell vector with a list of path/filenames. Each filename
+%   corresponds to a 2D slice of the 3D volume. The files can be any format
+%   that scimat_load() can read.
+%
+%   FROM/TO are 1- to 3-vectors with the (row, col, slice) indices of the
+%   cropping. NaN values can be provided to mean first/last voxel.
 
 % Authors: Ramon Casero <rcasero@gmail.com>, Benjamin Villard <b.016434@gmail.com>
-% Copyright © 2011-2015 University of Oxford
-% Version: 0.4.0
+% Copyright © 2011-2016 University of Oxford
+% Version: 0.5.0
 % 
 % University of Oxford means the Chancellor, Masters and Scholars of
 % the University of Oxford, having an administrative office at
@@ -62,74 +83,142 @@ function scimat = scimat_crop(scimat, from, to)
 % You should have received a copy of the GNU General Public License
 % along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-% check arguments
-narginchk(1, 3);
-nargoutchk(0, 1);
-
-% defaults
-if (nargin < 2)
-    from = [];
-end
-if (nargin < 3)
-    to = [];
-end
-
-% FROM and TO can be shorter than the number of dimensions of the image,
-% but not longer
-if (length(from) > ndims(scimat.data))
-    error('FROM has more dimensions than the image')
-end
-if (length(to) > ndims(scimat.data))
-    error('TO has more dimensions than the image')
-end
-
-% default: extend "from" and "to" with dummy dimensions, if necessary, so
-% that they are the same dimension as the image
-% E.g. in an image with size [256 512 128 10],
-%
-%     FROM=[50 75];
-%     TO=[100 89];
-%
-%   is the same as
-%
-%     FROM=[50 75 1 1];
-%     TO=[100 89 128 10];
-sz = size(scimat.data);
-sz(length(sz)+1:ndims(scimat.data)) = 1;
-from(length(from)+1:ndims(scimat.data)) = 1;
-to(length(to)+1:ndims(scimat.data)) = sz(length(to)+1:end);
-
-% "bottom-left" coordinates of what is going to become the first voxel of
-% the volume after cropping
-%
-% swap X and Y coordinates, so that they match the scimat convention for
-% the axes: (r, c, s, f) <-> (y, x, y, t)
-xmin = scimat_index2world(from, scimat);
-xmin = xmin([2 1 3:end]) - [scimat.axis.spacing]/2;
-
-% crop and correct metainformation in the scimat image
-for I = 1:length(scimat.axis)
+switch (class(scimat))
     
-    % crop one dimension of the image. We are going to be shifting
-    % circularly the dimensions of the image so that we always crop the
-    % first dimension
-    idx = cell(1, length(scimat.axis));
-    idx{1} = from(I):to(I);
-    idx(2:end) = {':'};
-    scimat.data = scimat.data(idx{:});
-    
-    % size
-    scimat.axis(I).size = size(scimat.data, 1);
-    
-    % "left" edge of first voxel
-    scimat.axis(I).min = xmin(I);
-    
-    % circular shift of the dimensions of image so that we can crop along
-    % the first dimension in the next iteration
-    idx = 1:length(scimat.axis);
-    scimat.data = permute(scimat.data, idx([2:end 1]));
-    
-end
+    case 'struct' % scimat structure
+        
+        % check arguments
+        narginchk(1, 3);
+        nargoutchk(0, 1);
+        
+        % defaults
+        if (nargin < 2)
+            from = [];
+        end
+        if (nargin < 3)
+            to = [];
+        end
+        
+        % FROM and TO can be shorter than the number of dimensions of the image,
+        % but not longer
+        if (length(from) > ndims(scimat.data))
+            error('FROM has more dimensions than the image')
+        end
+        if (length(to) > ndims(scimat.data))
+            error('TO has more dimensions than the image')
+        end
+        
+        % default: extend "from" and "to" with dummy dimensions, if necessary, so
+        % that they are the same dimension as the image
+        % E.g. in an image with size [256 512 128 10],
+        %
+        %     FROM=[50 75];
+        %     TO=[100 89];
+        %
+        %   is the same as
+        %
+        %     FROM=[50 75 1 1];
+        %     TO=[100 89 128 10];
+        sz = size(scimat.data);
+        sz(length(sz)+1:ndims(scimat.data)) = 1;
+        from(length(from)+1:ndims(scimat.data)) = 1;
+        to(length(to)+1:ndims(scimat.data)) = sz(length(to)+1:end);
+        
+        % "bottom-left" coordinates of what is going to become the first voxel of
+        % the volume after cropping
+        %
+        % swap X and Y coordinates, so that they match the scimat convention for
+        % the axes: (r, c, s, f) <-> (y, x, y, t)
+        xmin = scimat_index2world(from, scimat);
+        xmin = xmin([2 1 3:end]) - [scimat.axis.spacing]/2;
+        
+        % crop and correct metainformation in the scimat image
+        for I = 1:length(scimat.axis)
+            
+            % crop one dimension of the image. We are going to be shifting
+            % circularly the dimensions of the image so that we always crop the
+            % first dimension
+            idx = cell(1, length(scimat.axis));
+            idx{1} = from(I):to(I);
+            idx(2:end) = {':'};
+            scimat.data = scimat.data(idx{:});
+            
+            % size
+            scimat.axis(I).size = size(scimat.data, 1);
+            
+            % "left" edge of first voxel
+            scimat.axis(I).min = xmin(I);
+            
+            % circular shift of the dimensions of image so that we can crop along
+            % the first dimension in the next iteration
+            idx = 1:length(scimat.axis);
+            scimat.data = permute(scimat.data, idx([2:end 1]));
+            
+        end
+        
+        % Note: the last circular shift of the image dimension has already returned
+        % the image to its original shape, so no need to do anything after the loop
 
-% Note: the last circular shift of the image dimension has already returned
-% the image to its original shape, so no need to do anything after the loop
+    case 'cell' % list of files
+        
+        % check arguments
+        narginchk(1, 4);
+        nargoutchk(0, 1);
+        
+        if (nargin < 4 || isempty(ds))
+            ds = 1;
+        end
+        
+        % change nomenclature
+        file = scimat;
+        clear scimat
+        
+        % number of files
+        NS = length(file);
+        
+        if (NS == 0)
+            scimat = [];
+            return
+        end
+        
+        % load first file, to know the image size
+        scimat_in = scimat_load(file{1});
+        NR = size(scimat_in.data, 1);
+        NC = size(scimat_in.data, 2);
+        NCh = size(scimat_in.data, 5);
+        
+        % replace slice NaN indices by whole range of slices
+        if (length(from) < 1 || isnan(from(1))), from(1) = 1; end
+        if (length(to) < 1 || isnan(to(1))), to(1) = NR; end
+        if (length(from) < 2 || isnan(from(2))), from(2) = 1; end
+        if (length(to) < 2 || isnan(to(2))), to(2) = NC; end
+        if (length(from) < 3 || isnan(from(3))), from(3) = 1; end
+        if (length(to) < 3 || isnan(to(3))), to(3) = NS; end
+        
+        % offset of cropped volume
+        offset = scimat_index2world(from(1:2), scimat_in);
+        offset(3) = (from(3) - 1) * ds;
+        
+        % init output
+        scimat = scimat_im2scimat(...
+            zeros(to(1)-from(1)+1, to(2)-from(2)+1, to(3)-from(3)+1, 1, ...
+            NCh, 'like', scimat_in.data), ...
+            [scimat_in.axis.spacing ds], offset);
+        
+        % loop files
+        for I = from(3):to(3)
+            disp(['I = ' num2str(I)])
+            % load file
+            scimat_in = scimat_load(file{I});
+            
+            % transfer cropped slice
+            scimat.data(:, :, I-from(3)+1, :, :) ...
+                = scimat_in.data(from(1):to(1), from(2):to(2), :, :, :);
+            
+        end
+
+    otherwise
+        
+        error('First input must be a SCIMAT volume or a CELL vector with a list of filenames')
+        
+end
