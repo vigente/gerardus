db15df414bd6ab83ae98d3dae5a241e2b13a4c4c
Ramón Casero <rcasero@gmail.com>
2016-12-16 20:32:13 +0000

    (0.5.0): Add syntax for reference histogram.

--- matlab/CardiacToolbox/histology_preprocessing.m
+++ matlab/CardiacToolbox/histology_preprocessing.m
@@ -7,6 +7,11 @@ function [imref, im, mask] = histology_preprocessing(imref, im, opts)
 % matches the histograms to a reference slice. This prepares the slices for
 % successful intra-histology registration.
 %
+% The function has two syntaxes. The reference can be provided as an image
+% or histogram.
+%
+% -------------------------------------------------------------------------
+% Reference image syntax:
 % [IMREF2, IM2, MASK] = HISTOLOGY_PREPROCESSING(IMREF, IM)
 %
 %   IMREF is the slice whose histogram will be used as reference for the
@@ -45,12 +50,30 @@ function [imref, im, mask] = histology_preprocessing(imref, im, opts)
 %     'Grayscale': (def false) true/false whether image will be converted
 %              to grayscale.
 %
+% -------------------------------------------------------------------------
+% Reference histogram syntax:
+% [HREF2, IM2, MASK] = HISTOLOGY_PREPROCESSING(HREF, IM)
+%
+%   HREF is a (CH, L)-matrix with the reference histogram, where CH is the
+%   number of channels, and L is the number of intensity levels. For
+%   example, for an RGB image, CH=3, and for a uint8 class, L=256.
+%
+%   IM is the image to preprocess, as in the syntax above.
+%
+%   HREF2 is the histogram with the same preprocessing as the image.
+%
+%   IM2, MASK are as in the syntax above.
+%
+% ... = HISTOLOGY_PREPROCESSING(..., OPTS)
+%
+%   OPTS is as in the syntax above.
+%
 %
 % See also: regmatchedfilt, transfdiffreg.
 
 % Author: Ramon Casero <rcasero@gmail.com>
-% Copyright © 2014-2015 University of Oxford
-% Version: 0.4.2
+% Copyright © 2014-2016 University of Oxford
+% Version: 0.5.0
 % 
 % University of Oxford means the Chancellor, Masters and Scholars of
 % the University of Oxford, having an administrative office at
@@ -111,18 +134,22 @@ else % plain array
     
 end
 
-% convert IMREF to a plain array if it's not already
+% convert IMREF to a plain array if it's not already, or keep as histogram
+% if it's a histogram
 if (ischar(imref)) % filename
 
     imrefMat = scimat_load(imref);
     imrefMat = imrefMat.data;
     imrefType = 'filename';
 
-end
-if (isstruct(imref)) % scimat struct
+elseif (isstruct(imref)) % scimat struct
     
     imrefMat = imref.data;
     imrefType = 'scimat';
+    
+elseif (isa(imref, 'double') && size(imref, 1)==3) % histogram as (3,256) matrix
+    
+    imrefType = 'histogram';
     
 else % array
     
@@ -132,15 +159,29 @@ else % array
     
 end
 
-% grayscale conversion of IMREF, if requested by user
-if (opts.Grayscale)
-    
-    imrefMat = rgb2gray(squeeze(imrefMat));
-    
-end
+switch (imrefType)
+    case 'histogram'
+        
+        % is the histogram has more than one channel, we don't try to
+        % convert to grayscale
+        
+        % preprocessing of histogram equivalent to preprocessing the
+        % reference image (invert, threshold, etc.)
+        imref = histogram_preprocessing(imref);
+        
+    otherwise
+        
+        % grayscale conversion of IMREF, if requested by user
+        if (opts.Grayscale)
+            
+            imrefMat = rgb2gray(squeeze(imrefMat));
+            
+        end
 
-% preprocessing of IMREF
-imrefMat = individual_image_preprocessing(imrefMat);
+        % preprocessing of the reference image (invert, threshold, etc.)
+        imrefMat = individual_image_preprocessing(imrefMat);
+        
+end
 
 % if images are provided as filenames, either the user must provide an
 % output directory, or we generate a temp one with a random name
@@ -187,19 +228,32 @@ for I = 1:N
         
     end
     
-    % grayscale conversion of IM, if requested by user
-    if (opts.Grayscale)
-        
-        im0.data = rgb2gray(squeeze(im0.data));
-        
+    switch (imrefType)
+        case 'histogram'
+            
+            % preprocessing of IM
+            mask0 = im0;
+            [im0.data, mask0.data] = individual_image_preprocessing(im0.data);
+
+            im0.data = match_histograms_hist(imref, im0.data);
+            
+        otherwise
+            
+            % grayscale conversion of IM, if requested by user
+            if (opts.Grayscale)
+                
+                im0.data = rgb2gray(squeeze(im0.data));
+                
+            end
+            
+            % preprocessing of IM
+            mask0 = im0;
+            [im0.data, mask0.data] = individual_image_preprocessing(im0.data);
+            
+            % match IM histogram to IMREF
+            im0.data = match_histograms_im(imrefMat, im0.data);
+            
     end
-
-    % preprocessing of IMREF
-    mask0 = im0;
-    [im0.data, mask0.data] = individual_image_preprocessing(im0.data);
-
-    % match IM histogram to IMREF
-    im0.data = match_histograms(imrefMat, im0.data);
     
     % convert processed images to output format
     switch (imType)
@@ -309,15 +363,45 @@ mask = imdilate(mask, se);
 
 end
 
-% match_histograms:
+% histogram_preprocessing:
+%
+%   Transform histogram to replicate what individual_image_preprocessing()
+%   does to an image
+function hgram = histogram_preprocessing(hgram)
+
+% invert histogram, except for the pixels == 0, that continue being 0
+hgram(:, 2:end) = hgram(:, end:-1:2);
+
+% find the modes of the histogram
+[~, imode] = max(hgram, [], 2);
+
+% number of intensity levels
+L = size(hgram, 2);
+
+% loop histogram channels
+for CH = 1:size(hgram, 1)
+    
+    % crop the histogram <= mode
+    hgram(CH, 1) = sum(hgram(CH, 1:imode(CH)));
+    hgram(CH, 2:imode(CH)) = 0;
+    
+    % extend dynamic range of histogram
+    xbin = linspace(1, L-1, L-imode(CH));
+    hgram(CH, 2:end) = round(interp1(xbin, hgram(CH, (imode(CH)+1):end), 1:L-1));
+    
+end
+
+end
+
+% match_histograms_im:
 %
 %   Match the histogram of each channel in IM to IMREF, ignoring background
-%   voxels.
+%   voxels. IMREF is a reference image.
 %
 %   IMREF: reference image, array (R, C, CH)
-function im = match_histograms(imref, im)
+function im = match_histograms_im(imref, im)
 
-% match histograms to the central slice
+% match histograms to reference slice
 for I = 1:size(im, 5)
     
     % select channels
@@ -328,6 +412,32 @@ for I = 1:size(im, 5)
     idxref = chref > 0;
     idx = ch > 0;
     ch(idx) = imhistmatch(ch(idx), chref(idxref));
+    
+    % replace image channel with processed ones
+    %imref(:, :, I) = chref;
+    im(:, :, :, :, I) = ch;
+    
+end
+
+end
+
+% match_histograms_hist:
+%
+%   Match the histogram of each channel in IM to IMREF, ignoring background
+%   voxels. IMREF is a histogram with one or more channels.
+%
+%   IMREF: histogram, array (CH, intensity)
+function im = match_histograms_hist(hgram, im)
+
+% match image histograms to reference histogram
+for I = 1:size(im, 5)
+    
+    % select image channel
+    ch = im(:, :, :, :, I);
+    
+    % match histograms ignoring the background
+    idx = ch > 0;
+    ch(idx) = histeq(ch(idx), hgram(I, 2:end));
     
     % replace image channel with processed ones
     %imref(:, :, I) = chref;
