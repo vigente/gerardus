7ff0316fca78c8fc3069e6d45e562e3fba242929
Ramon Casero <rcasero@gmail.com>
2017-03-01 19:46:45 +0000

    shell-script/pre-commit-git-diff-docx.sh: (0.3.0) Fix bug. dtdoering's patch avoids trying to create the .md file with pandoc if the .docx has been deleted. It also deletes the .md file if the .docx file has been deleted. But the delete is not commited. Now we add the deleted .md file to .commit-amend-markdown so that the post-commit hook commits the delete. Also, only consider .docx to denote a Word file if it's at the end of the file's name.

--- shell-script/pre-commit-git-diff-docx.sh
+++ shell-script/pre-commit-git-diff-docx.sh
@@ -64,8 +64,8 @@
 # installed in the system.
 
 # Author: Ramon Casero <rcasero@gmail.com>
-# Version: 0.2.0
-# Copyright © 2016 University of Oxford
+# Version: 0.3.0
+# Copyright © 2016-2017 University of Oxford
 # 
 # University of Oxford means the Chancellor, Masters and Scholars of
 # the University of Oxford, having an administrative office at
@@ -104,7 +104,7 @@ cd `git rev-parse --show-toplevel`
 rm -f .commit-amend-markdown
 
 # create a Markdown copy of every .docx file that is committed, excluding deleted files
-for file in `git diff --cached --name-only --diff-filter=d | grep \.docx`
+for file in `git diff --cached --name-only --diff-filter=d | grep "\.docx$"`
 do
     # name of Markdown file
     mdfile="${file%.docx}.md"
@@ -126,12 +126,22 @@ do
 done
 
 # remove the Markdown copy of any file that is to be deleted from the repo
-for file in `git diff --cached --name-only --diff-filter=D | grep \.docx`
+for file in `git diff --cached --name-only --diff-filter=D | grep "\.docx$"`
 do
     # name of Markdown file
     mdfile="${file%.docx}.md"
     echo Removing Markdown copy of "$file"
 
-    rm "$mdfile"
+    if [ -e "$mdfile" ]
+       then
+	   # delete the Markdown file
+	   git rm "$mdfile"
+	   
+	   # list the Markdown files that need to be added to the
+	   # amended commit in the post-commit hook. Note that we
+	   # cannot `git add` here, because that adds the files to the
+	   # next commit, not to this one
+	   echo "$mdfile" >> .commit-amend-markdown
+    fi
 
 done
